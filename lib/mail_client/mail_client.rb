949f1621f29b806697f2336cd736942c38650335
Chris Recalis <crecalis@gmail.com>
2016-01-11 15:59:18 -0500

    switch to class style module for mail client

--- lib/mail_client/mail_client.rb
+++ lib/mail_client/mail_client.rb
@@ -1,28 +1,22 @@
 module MailClient
 
-    class ConfigurationError < Exception
-    end
+    class << self
+        attr_accessor :domain
 
-    def self.send(msg)
-        connection_pool.with do |conn|
-            conn.send_message(@@domain, msg)
+        def send(msg)
+            connection_pool.with do |conn|
+                conn.send_message(@domain, msg)
+            end
+        end
+
+        private
+
+        def connection_pool
+            @connection_pool ||= ConnectionPool.new(size: 1, timeout: 5) {
+                api_key = config["api_key"]
+                @domain = config["api_url"]
+                Mailgun::Client.new(api_key)
+            }
         end
     end
-
-    private
-
-    def self.set_default_domain(domain)
-        @@domain = domain
-    end
-
-    def self.connection_pool
-        @@connection_pool ||= ConnectionPool.new(size: 5, timeout: 5) {
-            config = Rails.configuration.mailgun
-            api_key = config["api_key"] || ""
-            domain = config["api_url"] || ""
-            set_default_domain(domain)
-            Mailgun::Client.new(api_key)
-        }
-    end
-
 end
