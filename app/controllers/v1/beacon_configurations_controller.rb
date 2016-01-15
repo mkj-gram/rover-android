5aa3baa4d34a9d692ea35bc4378548ee5b2976af
Chris Recalis <crecalis@gmail.com>
2016-01-15 15:58:58 -0500

    query builder for configurations

--- app/controllers/v1/beacon_configurations_controller.rb
+++ app/controllers/v1/beacon_configurations_controller.rb
@@ -2,28 +2,90 @@ class V1::BeaconConfigurationsController < V1::ApplicationController
     before_action :authenticate
 
     def index
-        configurations = BeaconConfiguration.__elasticsearch__.search(
+
+        filter = {
+            bool: {
+                should: [
+                    {
+                        term: {
+                            account_id: current_account.id
+                        }
+                    },
+                    {
+                        term: {
+                            shared_account_ids: current_account.id
+                        }
+                    }
+                ],
+                must: []
+            }
+        }
+
+        if !query_protocol.nil?
+            if query_protocol == "ibeacon"
+                filter[:bool][:must].push(
+                    {
+                        type: {
+                            value: "ibeacon_configuration"
+                        }
+                    }
+                )
+            elsif query_protocol == "eddystonenamespace"
+                filter[:bool][:must].push(
+                    {
+                        type: {
+                            value: "eddystone_namespace_configuration"
+                        }
+                    }
+                )
+            elsif query_protocol == "url"
+                filter[:bool][:must].push(
+                    {
+                        type: {
+                            value: "url_configuration"
+                        }
+                    }
+                )
+            end
+        end
+
+        querybuilder = {
             query: {
                 filtered: {
                     query: {
                         match_all: {}
                     },
-                    filter: {
-                        term: {
-                            account_id: current_account.id
-                        }
-                    }
+                    filter: filter
                 }
-            },
-            sort: [
-                {
-                    created_at: {
-                        order: "desc"
-                    }
-                }
-            ]
-        )
+            }
+        }
 
+
+        if !query_tags.nil?
+            filter[:bool][:must].push(
+                {
+                    terms: {
+                        tags: query_tags,
+                        execution: "and"
+                    }
+                }
+            )
+        end
+
+        if query_keyword.nil? && query_tags.nil?
+            querybuilder.merge!(
+                {
+                    sort: [
+                        {
+                            created_at: {
+                                order: "desc"
+                            }
+                        }
+                    ]
+                }
+            )
+        end
+        configurations = BeaconConfiguration.__elasticsearch__.search(querybuilder)
         results = configurations.per_page(page_size).page(current_page).results
         json  = {
             "data" => results.map do |config|
@@ -52,6 +114,22 @@ class V1::BeaconConfigurationsController < V1::ApplicationController
     end
 
     private
+
+    def filter_params
+        params.fetch(:filter, {}).permit(:protocol, {:tags => []}, :query)
+    end
+
+    def query_keyword
+        filter_params.fetch(:query, nil)
+    end
+
+    def query_tags
+        filter_params.fetch(:tags, nil)
+    end
+
+    def query_protocol
+        filter_params.fetch(:protocol, nil)
+    end
 
     def serialize_ibeacon(config)
         source = config._source
--- app/models/beacon_configurations/shared_beacon_configuration.rb
+++ app/models/beacon_configurations/shared_beacon_configuration.rb
@@ -3,4 +3,16 @@ class SharedBeaconConfiguration < ActiveRecord::Base
     belongs_to :owner_account, class_name: "Account", foreign_key: "owner_account_id"
     belongs_to :shared_account, class_name: "Account", foreign_key: "shared_account_id"
     belongs_to :beacon_configuration
+
+    after_create :re_index_beacon_configuration
+    after_create :re_index_beacon_configuration
+
+
+
+    private
+
+    def re_index_beacon_configuration
+        beacon_configuration.__elastic_search.update_document
+    end
+
 end
