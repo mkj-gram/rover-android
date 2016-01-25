68381dc3427d6b30ea87e378662ff9299183ef08
Chris Recalis <crecalis@gmail.com>
2016-01-25 10:24:10 -0500

    modify error message wording

--- app/models/user.rb
+++ app/models/user.rb
@@ -133,7 +133,7 @@ class User < ActiveRecord::Base
             errors.add(:current_password, "can't be blank")
         else
             if BCrypt::Password.new(password_digest_was) != current_password
-                errors.add(:current_password, "incorrect password")
+                errors.add(:current_password, "is incorrect")
             end
         end
     end
