e59656d6e2d6c57526b09ded02818fc78215de9b
Chris Recalis <crecalis@gmail.com>
2016-01-26 10:31:58 -0500

    make sure to render out errors with the email

--- app/error_serializers/v1/user_update_error_serializer.rb
+++ app/error_serializers/v1/user_update_error_serializer.rb
@@ -2,4 +2,6 @@ class V1::UserUpdateErrorSerializer < ModelError::Serializer
     attribute :current_password, error_key: "current-password"
     # attribute :unauthorized, error_key: "current-password"
     attribute :password
+
+    attribute :email
 end
