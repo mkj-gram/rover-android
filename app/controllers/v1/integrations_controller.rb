edb68b48b1bdad799c4b1c3dd5d7d97b18df7536
Chris Recalis <crecalis@gmail.com>
2016-01-27 14:26:33 -0500

    make sure to include the latest sync job as well

--- app/controllers/v1/integrations_controller.rb
+++ app/controllers/v1/integrations_controller.rb
@@ -67,6 +67,9 @@ class V1::IntegrationsController < V1::ApplicationController
                 json = {
                     "data" => serialize_integration(integration)
                 }
+
+                json["included"] = serialize_sync_job(integration, integration.latest_sync_job)
+
                 render json: json, status: :created
             else
                 render json: {errors: V1::ThirdPartyIntegrationsModelError.serialize(integration.errors)}, status: :unprocessable_entity
