c23f70fb3f42ba3b315e957f90bd28de9b606ba5
Chris Recalis <crecalis@gmail.com>
2016-01-27 14:23:25 -0500

    throw an error when the the client is unauthorized

--- lib/estimote_api/estimote_api.rb
+++ lib/estimote_api/estimote_api.rb
@@ -1,25 +1,12 @@
 require 'httparty'
+require 'lib/client'
 require 'lib/beacon'
+require 'lib/errors'
 
-class EstimoteApi
-    include HTTParty
-    base_uri "https://cloud.estimote.com/v1"
-    format :json
-    headers 'Accept' => 'application/json'
-
-    def initialize(app_id, app_token)
-        self.class.basic_auth(app_id, app_token)
-    end
-
-
-    def beacons(options = {})
-        response = self.class.get('/beacons', options)
-        if response.response.code == "200"
-            parsed_beacons = response.parsed_response
-            estimote_beacons = parsed_beacons.map!{|config| Beacon.new(config)}
-            return parsed_beacons
-        else
-            return []
+module EstimoteApi
+    class << self
+        def new(app_id, app_token)
+            EstimoteApi::Client.new(app_id, app_token)
         end
     end
 end
---
+++ lib/estimote_api/lib/errors.rb
@@ -0,0 +1,5 @@
+require 'lib/errors/unauthorized'
+module EstimoteApi
+    module Errors
+    end
+end
