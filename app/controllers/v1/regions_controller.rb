75b066fc61682ffd0f65d4c0874ebf062a56cddf
Chris Recalis <crecalis@gmail.com>
2016-01-28 14:32:31 -0500

    rename to an ibeacon-region

--- app/controllers/v1/regions_controller.rb
+++ app/controllers/v1/regions_controller.rb
@@ -4,9 +4,9 @@ class V1::RegionsController < V1::ApplicationController
 
 
     def index
-        beacon_regions = IBeaconConfiguration.select("DISTINCT on(uuid) *").where(account_id: current_account.id).all
+        ibeacon_regions = IBeaconConfiguration.select("DISTINCT on(uuid) *").where(account_id: current_account.id).all
         json = {
-            "data" => beacon_regions.map{|beacon_region| serialize_beacon_region(beacon_region)}
+            "data" => ibeacon_regions.map{|ibeacon_region| serialize_ibeacon_region(ibeacon_region)}
         }
 
         render json: json
@@ -16,20 +16,20 @@ class V1::RegionsController < V1::ApplicationController
 
     private
 
-    def serialize_beacon_region(beacon_region, opts = {})
+    def serialize_ibeacon_region(ibeacon_region, opts = {})
         include_major = opts.fetch(:include_major, false)
         include_minor = opts.fetch(:include_minor, false)
         json = {
-            "type" => "beacon-regions",
-            "id" => beacon_region.region_id(include_major: include_major, include_minor: include_minor),
+            "type" => "ibeacon-regions",
+            "id" => ibeacon_region.region_id(include_major: include_major, include_minor: include_minor),
             "attributes" => {
-                "uuid" => beacon_region.uuid,
+                "uuid" => ibeacon_region.uuid,
                 "major" => nil,
                 "minor" => nil
             }
         }
-        json["attributes"].merge!({"major" => beacon_region.major}) if include_major
-        json["attributes"].merge!({"minor" => beacon_region.minor}) if include_minor
+        json["attributes"].merge!({"major" => ibeacon_region.major}) if include_major
+        json["attributes"].merge!({"minor" => ibeacon_region.minor}) if include_minor
         return json
     end
 end
