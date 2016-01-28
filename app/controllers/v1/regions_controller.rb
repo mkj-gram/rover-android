47459956e0c645eea7006f195e3f879c1c58dcd3
Chris Recalis <crecalis@gmail.com>
2016-01-28 15:26:46 -0500

    render out a stub of geofences

--- app/controllers/v1/regions_controller.rb
+++ app/controllers/v1/regions_controller.rb
@@ -4,9 +4,61 @@ class V1::RegionsController < V1::ApplicationController
 
 
     def index
-        ibeacon_regions = IBeaconConfiguration.select("DISTINCT on(uuid) *").where(account_id: current_account.id).all
+        types = filter_types.dig(:types)
+
+
+        if types.include?("ibeacon")
+            ibeacon_regions = IBeaconConfiguration.select("DISTINCT on(uuid) *").where(account_id: current_account.id).limit(page_size)
+        else
+            ibeacon_region = []
+        end
+
+        if types.include?("geofence")
+            geofence_regions = [
+                {
+                    "type" => "geofence-regions",
+                    "id" => "43.7680567999999965:-79.4219654999999989",
+                    "attributes" => {
+                        "radius": 50,
+                        "longitude": -79.4219654999999989,
+                        "latitude": 43.7680567999999965
+                    }
+                },
+                {
+                    "type" => "geofence-regions",
+                    "id" => "43.7686240183319342:-79.4141962923431493",
+                    "attributes" => {
+                        "radius": 89,
+                        "longitude": -79.4141962923431493,
+                        "latitude": 43.7686240183319342
+                    }
+                },
+                {
+                    "type" => "geofence-regions",
+                    "id" => "43.7680567999999965:-79.4219654999999989",
+                    "attributes" => {
+                        "radius": 50,
+                        "longitude": -79.4219654999999989,
+                        "latitude": 43.7680567999999965
+                    }
+                },
+                {
+                    "type" => "geofence-regions",
+                    "id" => "43.7686240183319342:-79.4141962923431493",
+                    "attributes" => {
+                        "radius": 89,
+                        "longitude": -79.4141962923431493,
+                        "latitude": 43.7686240183319342
+                    }
+                }
+            ]
+        else
+            geofence_regions = []
+        end
+
+        data = ibeacon_regions.map{|ibeacon_region| serialize_ibeacon_region(ibeacon_region)} + geofence_regions
         json = {
-            "data" => ibeacon_regions.map{|ibeacon_region| serialize_ibeacon_region(ibeacon_region)}
+            "data" => data
         }
 
         render json: json
@@ -31,5 +83,15 @@ class V1::RegionsController < V1::ApplicationController
         json["attributes"].merge!({"major" => ibeacon_region.major}) if include_major
         json["attributes"].merge!({"minor" => ibeacon_region.minor}) if include_minor
         return json
+    end
+
+    def serialize_geofence_region(geofence_region)
+
+    end
+
+
+    def filter_types
+        # {filter: types: [ibeacon,geofence]}
+        params.fetch(:filter, {types: ["ibeacon", "geofence"]}).permit({:types => []})
     end
 end
