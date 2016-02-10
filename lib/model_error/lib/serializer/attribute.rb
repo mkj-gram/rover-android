46e374abe63fea07eb3e9dd87e580cce86a86377
Chris Recalis <crecalis@gmail.com>
2016-02-10 11:52:50 -0500

    update serializer to handle relationship errors

--- lib/model_error/lib/serializer/attribute.rb
+++ lib/model_error/lib/serializer/attribute.rb
@@ -2,32 +2,55 @@ module ModelError
     class Serializer
         class Attribute
             # defines the attribute to read from
-            def initialize(attribute, model_error)
+            def initialize(attribute, model_error, relationship = false)
                 @attribute = attribute
                 @model_error = model_error
+                @relationship = relationship
             end
 
             def serialize(key, errors)
                 serialized_errors = []
                 local_errors = errors[@attribute]
+                # key is the local key
                 local_errors.each do |error|
                     if @model_error
-                        serialized_errors.push({
-                                                 detail: error,
-                                                 source: {
-                                                     pointer: "data"
-                                                 }
-                        })
+                        serialized_errors.push(serialize_model_error(error))
+                    elsif @relationship
+                        serialized_errors.push(serialize_relationship_error(key, error))
                     else
-                        serialized_errors.push({
-                                                 detail: error,
-                                                 source: {
-                                                     pointer: "data/attributes/#{key}"
-                                                 }
-                        })
+                        serialized_errors.push(serialize_attribute_error(key, error))
                     end
                 end
                 return serialized_errors
+            end
+
+            private
+
+            def serialize_model_error(error)
+                {
+                    data: error,
+                    source: {
+                        pointer: "data"
+                    }
+                }
+            end
+
+            def serialize_attribute_error(key, error)
+                {
+                    detail: error,
+                    source: {
+                        pointer: "data/attributes/#{key}"
+                    }
+                }
+            end
+
+            def serialize_relationship_error(key, error)
+                {
+                    detail: error,
+                    source: {
+                        pointer: "data/relationships/#{key}"
+                    }
+                }
             end
         end
     end
--- lib/model_error/lib/serializer/attributes.rb
+++ lib/model_error/lib/serializer/attributes.rb
@@ -13,7 +13,8 @@ module ModelError
                 def attribute(attr, options = {})
                     key = options.fetch(:error_key, attr)
                     model_error = options.fetch(:model_error, false)
-                    _attributes_data[key] = Attribute.new(attr, model_error)
+                    relationship = options.fetch(:relationship, false)
+                    _attributes_data[key] = Attribute.new(attr, model_error, relationship)
                 end
             end
         end
