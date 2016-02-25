0f06418aa95056b4953f17b5b210686c7d94fc7f
Chris Recalis <crecalis@gmail.com>
2016-02-25 12:04:57 -0500

    make sure to convert to an integer

--- app/helpers/elasticsearch_shard_count_helper.rb
+++ app/helpers/elasticsearch_shard_count_helper.rb
@@ -4,7 +4,7 @@ module ElasticsearchShardCountHelper
         # settings { number_of_shards: 1, number_of_replicas: 2 }
         def get_settings(default_settings)
             if !@@config["max_shards_per_index"].nil?
-                shards_left = @@config["max_shards_per_index"]
+                shards_left = @@config["max_shards_per_index"].to_i
                 requested_number_of_shard = default_settings[:number_of_shards] || 1
                 requested_number_of_replicas = default_settings[:number_of_replicas] || 0
                 new_number_of_shards = [shards_left, requested_number_of_shard].min
