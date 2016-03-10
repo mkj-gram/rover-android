df10d030d6ce7ea502887da9455c639eda7ec7c0
Chris Recalis <crecalis@gmail.com>
2016-03-10 15:43:08 -0500

    check to see if the customer exists and has an inbox

--- app/controllers/v1/customer_inbox_controller.rb
+++ app/controllers/v1/customer_inbox_controller.rb
@@ -1,9 +1,13 @@
 class V1::CustomerInboxController < V1::ApplicationController
     before_action :authenticate
+    before_action :set_inbox
 
     def show
-        inbox = current_customer.inbox
-        messages = inbox.messages.select{|message| message.saved_to_inbox == true}.reverse!
+        if @inbox.nil?
+            messages = []
+        else
+            messages = @inbox.messages.select{|message| message.saved_to_inbox == true}.reverse!
+        end
         json = {
             data: messages.map{|message| V1::InboxMessageSerializer.serialize(message)}
         }
@@ -14,4 +18,11 @@ class V1::CustomerInboxController < V1::ApplicationController
 
     private
 
+    def set_inbox
+        if current_customer
+            @inbox = current_customer.inbox
+        else
+            head :not_found
+        end
+    end
 end
