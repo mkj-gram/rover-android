320e6f0374f53637e0213c05d98d1cad8d54f244
Chris Recalis <crecalis@gmail.com>
2016-03-18 15:02:31 -0400

    remove worker args

--- app/workers/update_customer_segments_count_worker.rb
+++ app/workers/update_customer_segments_count_worker.rb
@@ -1,12 +1,13 @@
 class UpdateCustomerSegmentsCountWorker
     include BackgroundWorker::Worker
 
-    from_queue 'update_customer_segments_count',
-        :durable => false,
-        :ack => true,
-        :threads => 1,
-        :prefetch => 1,
-        :timeout_job_after => 600
+    from_queue 'update_customer_segments_count'
+    # ,
+    #     :durable => false,
+    #     :ack => true,
+    #     :threads => 1,
+    #     :prefetch => 1,
+    #     :timeout_job_after => 600
 
     def self.preform_async
         msg = {}.to_json
