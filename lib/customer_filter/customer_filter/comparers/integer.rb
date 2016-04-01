653cc8cb0d56fcc77d297a4ec44ece2c5f541adb
Chris Recalis <crecalis@gmail.com>
2016-04-01 12:17:27 -0400

    fix integer range queries

--- lib/customer_filter/customer_filter/comparers/integer.rb
+++ lib/customer_filter/customer_filter/comparers/integer.rb
@@ -4,20 +4,8 @@ module CustomerFilter
 
             def initialize(opts)
                 super
-                @from = opts["from"].to_i
-                @to = opts["to"].to_i
-            end
-
-            def lower_bound
-                @from
-            end
-
-            def upper_bound
-                @to
-            end
-
-            def range?
-                lower_bound || upper_bound
+                @from = opts["from"]
+                @to = opts["to"]
             end
 
             def dump
@@ -26,14 +14,17 @@ module CustomerFilter
                     opts.delete("value")
                     opts.merge!(
                         {
-                            "from" => lower_bound,
-                            "to" => upper_bound
+                            "from" => @from,
+                            "to" => @to
                         }
                     )
                 end
                 return opts
             end
 
+            def range?
+                @from || @to
+            end
 
             def check(v)
                 case @method
@@ -54,170 +45,160 @@ module CustomerFilter
                 when Comparers::Methods::GREATER_THAN_OR_EQUAL
                     v >= @value
                 when Comparers::Methods::RANGE
+                    lower_bound = @from.nil? ? 0 : @from.to_i
+                    upper_bound = @to.nil? ? Float::INFINITY
                     Range.new(lower_bound, upper_bound).include?(v)
-                else
-                    false
                 end
 
-            end
+                def get_elasticsearch_query(attribute_name)
+                    case @method
+                    when Comparers::Methods::EQUAL
+                        {
+                            filter: {
+                                bool: {
+                                    must: [
+                                        {
+                                            term: {
+                                                attribute_name => @value
+                                            }
+                                        }
+                                    ]
+                                }
+                            }
+                        }
+                    when Comparers::Methods::NOT_EQUAL
+                        {
+                            filter: {
+                                bool: {
+                                    must_not: [
+                                        {
+                                            term: {
+                                                attribute_name => @value
+                                            }
+                                        }
+                                    ]
+                                }
+                            }
+                        }
+                    when Comparers::Methods::ANY_VALUE
+                        {}
+                    when Comparers::Methods::UNKNOWN_VALUE
+                        # when the value is neste
+                        # {
+                        #     filter: {
+                        #         nested: {
+                        #             path: "devices",
+                        #             filter: {
+                        #                 missing: {
+                        #                     field: attribute_name
+                        #                 }
+                        #             }
+                        #         }
+                        #     }
+                        # }
+                        {
+                            filter: {
+                                bool: {
+                                    must: [
+                                        {
+                                            missing: {
+                                                field: attribute_name
+                                            }
+                                        }
+                                    ]
+                                }
 
-            def extra_opts
-                if lower_bound && upper_bound
-                    {"from" => lower_bound, "to" => upper_bound}
-                else
-                    {}
+                            }
+                        }
+                    when Comparers::Methods::LESS_THAN
+                        {
+                            filter: {
+                                bool: {
+                                    must: [
+                                        {
+                                            range: {
+                                                attribute_name => {
+                                                    lt: @value
+                                                }
+                                            }
+                                        }
+                                    ]
+                                }
+                            }
+                        }
+                    when Comparers::Methods::LESS_THAN_OR_EQUAL
+                        {
+                            filter: {
+                                bool: {
+                                    must: [
+                                        {
+                                            range: {
+                                                attribute_name => {
+                                                    lte: @value
+                                                }
+                                            }
+                                        }
+                                    ]
+                                }
+
+                            }
+                        }
+                    when Comparers::Methods::GREATER_THAN
+                        {
+                            filter: {
+                                bool: {
+                                    must: [
+                                        {
+                                            range: {
+                                                attribute_name => {
+                                                    gt: @value
+                                                }
+                                            }
+                                        }
+                                    ]
+                                }
+
+                            }
+                        }
+                    when Comparers::Methods::GREATER_THAN_OR_EQUAL
+                        {
+                            filter: {
+                                bool: {
+                                    must: [
+                                        {
+                                            range: {
+                                                attribute_name => {
+                                                    gte: @value
+                                                }
+                                            }
+                                        }
+                                    ]
+                                }
+                            }
+                        }
+                    when Comparers::Methods::RANGE
+                        {
+                            filter: {
+                                bool: {
+                                    must: [
+                                        {
+                                            range: {
+                                                attribute_name => {
+                                                    from: @from,
+                                                    to: @to
+                                                }
+                                            }
+                                        }
+                                    ]
+                                }
+                            }
+                        }
+                    else
+                        {}
+                    end
                 end
+
+
             end
-
-            def get_elasticsearch_query(attribute_name)
-                case @method
-                when Comparers::Methods::EQUAL
-                    {
-                        filter: {
-                            bool: {
-                                must: [
-                                    {
-                                        term: {
-                                            attribute_name => @value
-                                        }
-                                    }
-                                ]
-                            }
-                        }
-                    }
-                when Comparers::Methods::NOT_EQUAL
-                    {
-                        filter: {
-                            bool: {
-                                must_not: [
-                                    {
-                                        term: {
-                                            attribute_name => @value
-                                        }
-                                    }
-                                ]
-                            }
-                        }
-                    }
-                when Comparers::Methods::ANY_VALUE
-                    {}
-                when Comparers::Methods::UNKNOWN_VALUE
-                    # when the value is neste
-                    # {
-                    #     filter: {
-                    #         nested: {
-                    #             path: "devices",
-                    #             filter: {
-                    #                 missing: {
-                    #                     field: attribute_name
-                    #                 }
-                    #             }
-                    #         }
-                    #     }
-                    # }
-                    {
-                        filter: {
-                            bool: {
-                                must: [
-                                    {
-                                        missing: {
-                                            field: attribute_name
-                                        }
-                                    }
-                                ]
-                            }
-
-                        }
-                    }
-                when Comparers::Methods::LESS_THAN
-                    {
-                        filter: {
-                            bool: {
-                                must: [
-                                    {
-                                        range: {
-                                            attribute_name => {
-                                                lt: @value
-                                            }
-                                        }
-                                    }
-                                ]
-                            }
-                        }
-                    }
-                when Comparers::Methods::LESS_THAN_OR_EQUAL
-                    {
-                        filter: {
-                            bool: {
-                                must: [
-                                    {
-                                        range: {
-                                            attribute_name => {
-                                                lte: @value
-                                            }
-                                        }
-                                    }
-                                ]
-                            }
-
-                        }
-                    }
-                when Comparers::Methods::GREATER_THAN
-                    {
-                        filter: {
-                            bool: {
-                                must: [
-                                    {
-                                        range: {
-                                            attribute_name => {
-                                                gt: @value
-                                            }
-                                        }
-                                    }
-                                ]
-                            }
-
-                        }
-                    }
-                when Comparers::Methods::GREATER_THAN_OR_EQUAL
-                    {
-                        filter: {
-                            bool: {
-                                must: [
-                                    {
-                                        range: {
-                                            attribute_name => {
-                                                gte: @value
-                                            }
-                                        }
-                                    }
-                                ]
-                            }
-                        }
-                    }
-                when Comparers::Methods::RANGE
-                    {
-                        filter: {
-                            bool: {
-                                must: [
-                                    {
-                                        range: {
-                                            attribute_name => {
-                                                from: lower_bound,
-                                                to: upper_bound
-                                            }
-                                        }
-                                    }
-                                ]
-                            }
-                        }
-                    }
-                else
-                    {}
-                end
-            end
-
-
         end
     end
-end
