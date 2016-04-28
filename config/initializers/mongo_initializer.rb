28a164c07c4421d3a68f632d96de6c337a19cb0f
Chris Recalis <crecalis@gmail.com>
2016-04-28 09:04:15 -0400

    seperate the indexes into a rake task

--- config/initializers/mongo_initializer.rb
+++ config/initializers/mongo_initializer.rb
@@ -16,29 +16,3 @@ mongo_config = (config.merge(defaults)).symbolize_keys
 uri = mongo_config.delete(:uri)
 
 $mongo = Mongo::Client.new(uri, mongo_config)
-
-
-$mongo[Customer.collection_name].indexes.create_many(
-    [
-        { :key => { :account_id =>  1, :"devices._id" => 1}, :unique => true, :partial_filter_expression => { :"devices._id" => { :"$exists" => true } } },
-        { :key => { :account_id =>  1, :identifier => 1 }, :unique => true, :partial_filter_expression => { :"identifier" => { :"$exists" => true } } },
-        { :key => { :"devices.token" => 1 }, :unique => true, :partial_filter_expression => { :"devices.token" => { :"$exists" => true } } }
-    ]
-)
-
-$mongo[Message.collection_name].indexes.create_many(
-    [
-        { :key => { :expire => 1 }, :sparse => true, :expire_after_seconds => 0}
-    ]
-)
-
-
-# create all indexes
-# customers
-# index({"account_id": 1, "devices._id": 1}, {unique: true,partial_filter_expression {"devices._id" => {"$exists" => true}}})
-# index({"account_id": 1, "identifier": 1},  {unique: true,partial_filter_expression {"identifier" => {"$exists" => true}}})
-# index({"account_id": 1, "traits": 1}, partial_filter_expression {"traits" => {"$exists" => true}}})
-# index({"devices.token": 1}, {unique: true,partial_filter_expression {"devices.token" => {"$exists" => true}}})
-#
-# messages
-# # index({ expire_at: 1 }, { sparse: true,  expire_after_seconds: 0 })
