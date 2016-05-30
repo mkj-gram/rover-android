81a6546a4d7ce0d9c1d97c642ac8710b16fdf4f0
Chris Recalis <crecalis@gmail.com>
2016-05-30 12:48:20 -0400

    border should have a radius

--- app/models/landing_page_builder/concerns/has_border.rb
+++ app/models/landing_page_builder/concerns/has_border.rb
@@ -7,12 +7,14 @@ module LandingPageBuilder
                 attribute :border_color, LandingPageBuilder::Color, default: { red: 129, green: 129, blue: 129 }
                 attribute :border_width, Float, default: 1
                 attribute :border_height, Float, default: 0
+                attribute :border_radius, Integer
 
                 def border_equal?(other)
                     (
                         border_color == other.border_color &&
                         border_width == other.border_width &&
-                        border_height == other.border_height
+                        border_height == other.border_height &&
+                        border_radius == other.border_radius
                     )
                 end
             end
