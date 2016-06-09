10257988b51379255ffdbcf79b6825f40f36c17f
Chris Recalis <crecalis@gmail.com>
2016-06-09 10:47:15 -0400

    fix migration instructions to not throw an error

--- db/migrate/20160609142610_add_timestamps_to_sessions.rb
+++ db/migrate/20160609142610_add_timestamps_to_sessions.rb
@@ -1,5 +1,8 @@
 class AddTimestampsToSessions < ActiveRecord::Migration
     def change
-        add_timestamps(:sessions, null: false)
+    	add_column :sessions, :created_at, :datetime
+    	add_column :sessions, :updated_at, :datetime
+    	change_column_null :sessions, :created_at, false
+    	change_column_null :sessions, :updated_at, false
     end
 end
