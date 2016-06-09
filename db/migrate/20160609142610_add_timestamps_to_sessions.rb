93d18a346bd31e064e98b6f9f9f5b67baf2e267b
Chris Recalis <crecalis@gmail.com>
2016-06-09 10:56:19 -0400

    make sure all old sessions have a default value

--- db/migrate/20160609142610_add_timestamps_to_sessions.rb
+++ db/migrate/20160609142610_add_timestamps_to_sessions.rb
@@ -1,8 +1,16 @@
 class AddTimestampsToSessions < ActiveRecord::Migration
-    def change
-    	add_column :sessions, :created_at, :datetime
+
+	def up
+		add_column :sessions, :created_at, :datetime
     	add_column :sessions, :updated_at, :datetime
-    	change_column_null :sessions, :created_at, false
+		Session.update_all(created_at: Time.zone.now, updated_at: Time.zone.now)
+		change_column_null :sessions, :created_at, false
     	change_column_null :sessions, :updated_at, false
-    end
+	end
+
+	def down
+		remove_column :sessions, :created_at
+		remove_column :sessions, :updated_at
+	end
+	
 end
