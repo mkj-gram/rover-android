fcdc3d1573ff233b6f0e48f67544a9be36deedec
Chris Recalis <crecalis@gmail.com>
2016-06-13 16:59:16 -0400

    split the sync jobs to their respective types

--- db/migrate/20160608200150_convert_old_sync_jobs_to_beacon_sync_jobs.rb
+++ db/migrate/20160608200150_convert_old_sync_jobs_to_beacon_sync_jobs.rb
@@ -1,7 +1,16 @@
 class ConvertOldSyncJobsToBeaconSyncJobs < ActiveRecord::Migration
 
-   	def up
-   		ThirdPartyIntegrationSyncJob.where(type: nil).update_all(type: "BeaconSyncJob")
+    def up
+        ThirdPartyIntegrationSyncJob.where(type: nil).includes(:third_party_integration).each do |sync_job|
+            case sync_job.third_party_integration
+            when EstimoteIntegration
+                sync_job.update(type: "EstimoteSyncJob")
+            when KontaktIntegration
+                sync_job.update(type: "KontaktSyncJob")
+            else
+                sync_job.update(type: "BeaconSyncJob")
+            end
+        end
     end
 
 end
