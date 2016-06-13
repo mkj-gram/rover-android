44b29ce31a8064edcd235d704716a3228957ab0b
Chris Recalis <crecalis@gmail.com>
2016-06-13 17:08:39 -0400

    update routes

--- app/controllers/v1/integrations/sync_jobs_controller.rb
+++ app/controllers/v1/integrations/sync_jobs_controller.rb
@@ -60,20 +60,18 @@ class V1::Integrations::SyncJobsController < V1::ApplicationController
     def get_integration_id
         @integration_id ||= -> {
             id = params[:integration_id] || params[:estimote_integration_id] || params[:kontakt_integration_id] || params[:gimbal_integration_id] || params[:google_integration_id]
+            
             if id.nil?
                 # this request might have the integrations id in the payload
                 json = flatten_request({single_record: true})
                 id = json.dig(:data, :sync_jobs, :integration_id)
-                
-                if id.nil?
-                    id = json.dig(:data, :beacon_sync_jobs, :integration_id)
-                end
 
-                if id.nil?
-                    id = json.dig(:data, :google_sync_jobs, :integration_id)
-                end
-
+                id = json.dig(:data, :estimote_sync_jobs, :integration_id) if id.nil?
+                id = json.dig(:data, :kontakt_sync_jobs, :integration_id) if id.nil?
+                id = json.dig(:data, :beacon_sync_jobs, :integration_id) if id.nil?
+                id = json.dig(:data, :google_sync_jobs, :integration_id) if id.nil?
             end
+
             return id
         }.call
     end
