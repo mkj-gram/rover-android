bfcaf1f0ffb7a4a3dcb986fc6d606993c1e78952
Chris Recalis <crecalis@gmail.com>
2016-06-16 12:14:21 -0400

    use long running connections

--- app/helpers/apns_helper.rb
+++ app/helpers/apns_helper.rb
@@ -2,27 +2,40 @@ module ApnsHelper
 
     class << self
 
-        def send(apns_app, messages_by_token, devices)
-            return [] if devices.nil? || devices.empty?
+        def send(apns_app, messages_by_token)
+            return [] if messages_by_token.empty?
             certificate = ApnsKit::Certificate.from_p12_file(apns_app.certificate, apns_app.passphrase)
             client = ApnsKit::Client.production(certificate, pool_size: 1, heartbeat_interval: 0)
 
-            notifications = []
-
-            devices.each do |device|
-                messages = messages_by_token[device.token]
-                messages.each do |message|
-                    payload = payload_from_message(message)
-                    notifications.push(ApnsKit::Notification.new(token: device.token, sound: message.ios_sound_file, alert: { title: message.ios_title, body: message.notification_text }, data: payload))
-                end
-            end
-
-            responses = client.send(notifications)
+            notifications = messages_to_notifications(messages_by_token)
+            
+            responses = send_with_connection(client, notifications)
             expired_tokens = responses.select{ |response| response.invalid_token? }.map{ |response| response.notification.token }
 
             client.shutdown
 
             return expired_tokens
+        end
+
+        def messages_to_notifications(messages_by_token)
+            notifications = []
+            messages_by_token.each do |token, messages|
+                messages.each do |message|
+                    payload = payload_from_message(message)
+                    notifications.push(ApnsKit::Notification.new(token: device.token, sound: message.ios_sound_file, alert: { title: message.ios_title, body: message.notification_text }, data: payload ))
+                end
+            end
+        end
+
+        def connection_from_ios_platform(ios_platform, opts = {})
+            return nil if ios_platform.certificate.nil?
+            certificate = ApnsKit::Certificate.from_p12_file(ios_platform.certificate, ios_platform.passphrase)
+            client = ApnsKit::Client.production(certificate, opts)
+            return client
+        end
+
+        def send_with_connection(connection, notifications)
+            return connection.send(notifications)
         end
 
         private
--- app/workers/send_message_notifications_worker.rb
+++ app/workers/send_message_notifications_worker.rb
@@ -5,18 +5,65 @@ class SendMessageNotificationWorker
         :prefetch => 15,
         :ack => true
 
+    class << self
+        def perform_async(customer_id, message_ids = [], device_ids_filter = [])
+            return if customer_id.nil? || message_ids.empty?
 
-    def self.perform_async(customer_id, message_ids = [], device_ids_filter = [])
-        return if customer_id.nil? || message_ids.empty?
+            msg = {
+                customer_id: customer_id,
+                message_ids: message_ids,
+                device_ids_filter: device_ids_filter
+            }
 
-        msg = {
-            customer_id: customer_id,
-            message_ids: message_ids,
-            device_ids_filter: device_ids_filter
-        }
+            enqueue_message(msg, {to_queue: 'send_message_notifications'})
+        end
 
-        enqueue_message(msg, {to_queue: 'send_message_notifications'})
+        def connection_pool
+            @@connection_pool
+        end
     end
+
+    class ApnsLongTermConnectionPool
+
+        # defaults to 10 active connections
+        def initialize(size: 10)
+            Rails.logger.info("Initializing connection pool of size #{size}")
+            @size = size
+            @connections = {}
+            @mutex = Mutex.new
+            @last_id = nil
+        end
+
+
+        def get(id)
+            @mutex.synchronize do
+                @connections[id]
+            end
+        end
+
+        def add(id, connection)
+            if @connections.size > @size
+                remove(@connections.keys.last)
+            end
+
+            @mutex.synchronize do
+                Rails.logger.info("Adding connection with id #{id}")
+                @connections[id] = connection
+            end
+        end
+
+        def remove(id)
+            @mutex.synchronize do
+                Rails.logger.info("Removing connection with id #{id}")
+                connection = @connections[id]
+                connection.shutdown if connection
+                connection = nil
+                @connections.delete(id)
+            end
+        end
+    end
+
+    @@connection_pool = ApnsLongTermConnectionPool.new(size: 20)
 
     def perform(args)
         customer_id = args["customer_id"]
@@ -25,27 +72,48 @@ class SendMessageNotificationWorker
 
         customer = Customer.find(customer_id)
         messages = Message.find_all(message_ids)
-        ios_platform = IosPlatform.find(customer.account_id)
 
-        if ios_platform
-            ios_devices = customer.devices.select { |device| device.os_name == "iOS" && device.remote_notifications_enabled }
-            ios_devices.select! { |device| device_ids_filter.include? (device.id) } if device_ids_filter
-
-            message_ids_by_token = ios_devices.inject({}) { |hash, device| hash.merge!(device.token => messages); hash }
-
-            # TODO
-            # setup long lived connections for these
-            expired_tokens = ApnsHelper.send(ios_platform, message_ids_by_token, ios_devices)
-
-            # need a way to specify the device without updating the customer
-            # TODO:!!!!!!
-            # if expired_tokens && expired_tokens.any?
-            #     expired_devices = ios_devices.select { |device| expired_tokens.include?(expired_tokens) }
-            #     expired_devices.each do |device|
-            #         device.update_attributes()
-            #     end
-            # end
+        if @@connection_pool.get(customer.account_id).nil?
+            ios_platform = IosPlatform.find(customer.account_id)
+            return ack! if ios_platform.nil?
+            connection = ApnsHelper.connection_from_ios_platform(ios_platform, pool_size: 1, heartbeat_interval: 120)
+            @@connection_pool.add(customer.account_id, connection)
         end
+
+        ios_devices = customer.devices.select { |device| device.os_name == "iOS" && device.remote_notifications_enabled }
+        ios_devices.select! { |device| device_ids_filter.include? (device.id) } if device_ids_filter
+
+        messages_by_token = ios_devices.inject({}) { |hash, device| hash.merge!(device.token => messages); hash }
+
+        connection = @@connection_pool.get(customer.account_id)
+
+        notifications = ApnsHelper.messages_to_notifications(messages_by_token)
+
+        responses = ApnsHelper.send_with_connection(connection, notifications)
+
+        Rails.logger.info(responses)
+
+        # ios_platform = IosPlatform.find(customer.account_id)
+
+        # if ios_platform
+
+
+        #     connection = connection_from_ios_platform(ios_platform)
+        #     notifications =
+
+        #         # TODO
+        #         # setup long lived connections for these
+        #         expired_tokens = ApnsHelper.send(ios_platform, message_ids_by_token, ios_devices)
+
+        #     # need a way to specify the device without updating the customer
+        #     # TODO:!!!!!!
+        #     # if expired_tokens && expired_tokens.any?
+        #     #     expired_devices = ios_devices.select { |device| expired_tokens.include?(expired_tokens) }
+        #     #     expired_devices.each do |device|
+        #     #         device.update_attributes()
+        #     #     end
+        #     # end
+        # end
 
         ack!
     end
