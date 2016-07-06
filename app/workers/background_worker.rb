cb3724e3a046c8318fa62f6d769ea423b3fedce2
Chris Recalis <crecalis@gmail.com>
2016-07-06 10:39:48 -0400

    fix

--- app/workers/background_worker.rb
+++ app/workers/background_worker.rb
@@ -49,7 +49,7 @@ module BackgroundWorker
                     return :ack
                 rescue Exception => e
                     Rails.logger.info("FAILED: #{self.class.name} failed to perform #{msg}")
-                    Raven.capture_exception(exception)
+                    Raven.capture_exception(e)
                     raise e
                 end
             end
