20ef5d1172560d026af70c21705f282b205a723e
Chris Recalis <crecalis@gmail.com>
2016-07-12 16:03:19 -0400

    default border radius to 0

--- app/models/landing_page_builder/concerns/has_border.rb
+++ app/models/landing_page_builder/concerns/has_border.rb
@@ -7,7 +7,7 @@ module LandingPageBuilder
                 attribute :border_color, LandingPageBuilder::Color, default: { red: 129, green: 129, blue: 129 }
                 attribute :border_width, Float, default: 1
                 attribute :border_height, Float, default: 0
-                attribute :border_radius, Integer
+                attribute :border_radius, Integer, default: 0
 
                 def border_equal?(other)
                     (
