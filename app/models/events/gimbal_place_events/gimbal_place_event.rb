fbd34a98d6dcf627d48d1593e2d35b5f54c58d19
Chris Recalis <crecalis@gmail.com>
2016-07-21 13:02:08 -0400

    let the query perform the filter

--- app/models/events/gimbal_place_events/gimbal_place_event.rb
+++ app/models/events/gimbal_place_events/gimbal_place_event.rb
@@ -27,11 +27,11 @@ module Events
             end
 
             def get_messages
-                message_templates = ProximityMessageTemplate.where(account_id: account.id, published: true, filter_gimbal_place_id: gimbal_place_id, trigger_event_id: self.class.event_id).where(today_schedule_column => true).where("? <@ date_schedule", generation_time_date).where("? <@ time_schedule", generation_time_minutes_since_midnight).all.to_a
+                message_templates = ProximityMessageTemplate.where(account_id: account.id, published: true, filter_gimbal_place_id: [nil, gimbal_place_id], trigger_event_id: self.class.event_id).where(today_schedule_column => true).where("? <@ date_schedule", generation_time_date).where("? <@ time_schedule", generation_time_minutes_since_midnight).all.to_a
                 # apply all filters
                 current_time = DateTime.now
                 message_templates.select do |message_template|
-                    message_template.within_schedule(current_time) && message_template.apply_gimbal_filters(gimbal_place) && message_template.within_customer_segment(customer, device)
+                    message_template.within_schedule(current_time) && message_template.within_customer_segment(customer, device)
                 end
             end
 
