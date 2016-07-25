4484eb62b864e73d284e964136167264419cd818
Chris Recalis <crecalis@gmail.com>
2016-07-25 15:40:48 -0400

    return the type property for gimbal places

--- app/controllers/v1/gimbal_places_controller.rb
+++ app/controllers/v1/gimbal_places_controller.rb
@@ -111,6 +111,7 @@ class V1::GimbalPlacesController < V1::ApplicationController
     def serialize_gimbal_place(gimbal_place)
         {
             id: gimbal_place.id.to_s,
+            type: 'gimbal-places'
             attributes: {
                 name: gimbal_place.name,
             }
@@ -121,6 +122,7 @@ class V1::GimbalPlacesController < V1::ApplicationController
         source = gimbal_place._source
         {
             id: gimbal_place.id,
+            type: 'gimbal-places'
             attributes: {
                 name: source.name
             }
