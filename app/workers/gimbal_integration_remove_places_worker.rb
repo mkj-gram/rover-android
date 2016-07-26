75fd693848a93f97434d9a340989e7946e7546d7
Chris Recalis <crecalis@gmail.com>
2016-07-26 16:12:32 -0400

    only remove old gimbal places from search
    If a message is triggered against a gimbal place and the integration which imported the place has been deleted, we still want to render out what places the message is targetting

--- app/workers/gimbal_integration_remove_places_worker.rb
+++ app/workers/gimbal_integration_remove_places_worker.rb
@@ -11,10 +11,24 @@ class GimbalIntegrationRemovePlacesWorker
     def perform(args)
         # grab the integration and sync it
         integration_id = args["id"]
+        bulk = []
 
-        GimbalPlace.where(gimbal_integration_id: integration_id).destroy_all
-        
-        ack!
+        GimbalPlace.where(gimbal_integration_id: integration_id).find_in_batches(batch_size: 500) do |gimbal_places|
+            gimbal_places.each do |gimbal_place|
+                bulk.push({
+                    delete: { _index: GimbalPlace.index_name, _type: GimbalPlace.document_type, _id: gimbal_place.id }
+                })
+            end
+        end
+
+        begin
+            Elasticsearch::Model.client.bulk(body: bulk)
+            return ack!
+        rescue Exception => e
+            Raven.capture_exception(exception)
+            Sneakers.logger.warn(e)
+            return reject!
+        end
 
     end
 end
