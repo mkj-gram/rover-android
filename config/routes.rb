eaf688ea86c02df34c48d10bffc003bb3f7b7c60
Chris Recalis <crecalis@gmail.com>
2016-08-12 14:24:49 -0400

    make sure to grab the experience id from the route

--- config/routes.rb
+++ config/routes.rb
@@ -135,8 +135,8 @@ Rails.application.routes.draw do
 
         get '/experiences/:short_url', to: 'experiences#short_url', constraints: { short_url: /\w{6}/ }
         resources :experiences, only: [:index, :show, :create, :update, :destroy]
-        patch '/experiences/publish', to: 'experiences#publish'
-        patch '/experiences/revert', to: 'experiences#revert'
+        patch '/experiences/:id/publish', to: 'experiences#publish'
+        patch '/experiences/:id/revert', to: 'experiences#revert'
     end
     # The priority is based upon order of creation: first created -> highest priority.
     # See how all your routes lay out with "rake routes".
