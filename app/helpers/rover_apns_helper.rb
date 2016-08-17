dae02a7896450b2a8932a16d4b9247746613e1fe
Chris Recalis <crecalis@gmail.com>
2016-08-17 10:52:23 -0400

    make sure the rover connection uses a heartbeat interval

--- app/helpers/rover_apns_helper.rb
+++ app/helpers/rover_apns_helper.rb
@@ -3,7 +3,7 @@ module RoverApnsHelper
 	class << self
 
 		def get_connection
-            @connection ||= ApnsKit::Client.production(get_certificate, pool_size: 1, heartbeat_interval: 0)
+            @connection ||= ApnsKit::Client.production(get_certificate, pool_size: 1, heartbeat_interval: 60)
 		end
 
 		def get_certificate
