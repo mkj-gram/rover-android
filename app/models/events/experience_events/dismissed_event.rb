2c43764641db35fc383aa3b10c118cae2a9bd0ad
Chris Recalis <crecalis@gmail.com>
2016-08-19 14:49:32 -0400

    use the new variable

--- app/models/events/experience_events/dismissed_event.rb
+++ app/models/events/experience_events/dismissed_event.rb
@@ -4,7 +4,7 @@ module Events
         class ClosedEvent < ExperienceEvent
 
             def self.event_id
-                Events::Constants::EXPERIENCE_CLOSED_EVENT_ID
+                Events::Constants::EXPERIENCE_DISMISSED_EVENT_ID
             end
 
             Events::Pipeline.register("experience", "dismissed", self, { targetable: false })
