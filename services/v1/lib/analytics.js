d40326be274858446d6edbedf319dc87ebecbcad
Chris Recalis <crecalis@gmail.com>
2016-10-13 08:57:49 -0400

    guard against events too far in the future

--- services/v1/lib/analytics.js
+++ services/v1/lib/analytics.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const internals = {};
-
+const TWELEVE_HOURS = 43200;
 
 internals.getTag = function(event) {
     return `account_${event._account.id}.event`;
@@ -12,12 +12,19 @@ internals.capture = function(event) {
     const fluentd = server.connections.fluentd.logger;
     const logger = server.plugins.logger.logger;
 
+    let timestamp = event.utcUnixTimestamp();
     let tag = internals.getTag(event);
     let record = event.toRecord();
 
+    if (timestamp > Math.floor(Date.now() / 1000) + TWELEVE_HOURS ) {
+        logger.warn("Event was generated too far in the future: " + tag);
+        logger.warn(record);
+        return;
+    }
+
     logger.debug(`Service: [analytics.capture]: ${tag}`);
     logger.debug(JSON.stringify(record));
-    fluentd.emit(tag, record, event.utcUnixTimestamp());
+    fluentd.emit(tag, record, timestamp);
 };
 
 module.exports = {
