78ad67dddfdb3c286e06e6440ee5bf029ec0cafc
Chris Recalis <crecalis@gmail.com>
2016-10-20 14:50:42 -0400

    use template instead of message

--- controllers/v1/inbox-landing-page.js
+++ controllers/v1/inbox-landing-page.js
@@ -22,28 +22,29 @@ internals.get = function(request, reply) {
     const messageId = request.params.id;
 
     methods.application.getCurrentCustomer(request, (err, customer) => {
-        methods.message.find(messageId, { fields: ['landing_page', 'customer_id']}, (err, message) => {
+        methods.message.find(messageId, { fields: ['customer_id', 'message_template_id']}, (err, message) => {
             // we are only requesting for the landing_page field to be set
             if (err) {
                 return internals.writeError(reply, 400, { status: 404, error: 'message doesn\'t exist'});
             }
 
             if ( customer._id.equals(message.customer_id) ){
-               
-                if (util.isUndefined(message.landing_page)) {
-                    reply.writeHead(204);
-                    return reply.end();
-                } else {
-                    let response = JSON.stringify(internals.serialize(message));
+                methods.messageTemplate.find(message.message_template_id, { useCache: true }, (err, template) => {
+                    if (util.isUndefined(template)) {
+                        reply.writeHead(204);
+                        return reply.end();
+                    } else {
+                        let response = JSON.stringify(internals.serialize(template));
 
-                    reply.writeHead(200, {
-                        'Content-Type': 'application/json',
-                        'Connection': 'keep-alive',
-                        'Content-Length': Buffer.byteLength(response, "utf-8")
-                    });
-                    reply.write(response);
-                    return reply.end();
-                } 
+                        reply.writeHead(200, {
+                            'Content-Type': 'application/json',
+                            'Connection': 'keep-alive',
+                            'Content-Length': Buffer.byteLength(response, "utf-8")
+                        });
+                        reply.write(response);
+                        return reply.end();
+                    } 
+                });
             } else {
                 return internals.writeError(reply, 403, { status: 403, error: 'You do not have access to this message'});
             }   
@@ -51,8 +52,8 @@ internals.get = function(request, reply) {
     });
 };
 
-internals.serialize = function(message) {
-    return dasherize(message.landing_page);
+internals.serialize = function(template) {
+    return dasherize(template.landing_page_template);
 };
 
 
