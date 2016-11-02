9ec5c52060a92d84fb123424526cd1637e82698b
Chris Recalis <crecalis@gmail.com>
2016-11-02 13:10:27 -0400

    do not set tags to empty if they haven’t been set

--- controllers/v1/event.js
+++ controllers/v1/event.js
@@ -756,10 +756,12 @@ internals.parseCustomerPayload = function(accountId, payload) {
         delete customer.identifier;
     }
 
-    if (customer.tags && util.isArray(customer.tags)) {
-        customer.tags = customer.tags.filter(tag => { return (!util.isNullOrUndefined(tag) && util.isString(tag) && tag.length > 0) });
-    } else {
-        customer.tags = [];
+    if (!util.isNullOrUndefined(customer.tags)) {
+        if (util.isArray(customer.tags)) {
+            customer.tags = customer.tags.filter(tag => { return (!util.isNullOrUndefined(tag) && util.isString(tag) && tag.length > 0) });
+        } else {
+            customer.tags = [];
+        }
     }
 
     internals.parseValue(customer, 'first_name', Type.STRING);
