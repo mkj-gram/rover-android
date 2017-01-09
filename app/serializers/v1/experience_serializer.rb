6c279a48c608c817224227e4ff2628f228025668
Chris Recalis <crecalis@gmail.com>
2017-01-09 12:49:36 -0500

    render simulator-url on all experiences

--- app/serializers/v1/experience_serializer.rb
+++ app/serializers/v1/experience_serializer.rb
@@ -1,7 +1,10 @@
 module V1::ExperienceSerializer
     class << self
-        def serialize(experience, version, opts = {})
+        def serialize(experience, version, subdomain = "", opts = {})
             has_unpublished_changes = !experience.current_version_id.nil?
+
+            simulator_url = subdomain.empty? ? "https://rvr.co/#{experience.short_url}" : "https://#{subdomain}.rvr.co/#{experience.short_url}"
+
             data = {
                 id: experience.id.to_s,
                 type: 'experiences'.freeze,
@@ -12,6 +15,7 @@ module V1::ExperienceSerializer
                     :'is-published' => experience.is_published,
                     :'is-archived' => experience.is_archived,
                     :'short-url' => experience.short_url,
+                    :'simulator-url' => simulator_url,
                     :'home-screen-id' => version.nil? ? nil : version.home_screen_id,
                     screens: version.nil? ? [] : version.screens.as_json.map{|screen| screen.deep_transform_keys{|key| key.to_s.dasherize }}
                 }
