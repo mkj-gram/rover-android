626f2ec9c8571e70d909980bba9a29f5d3cc1028
Chris Recalis <crecalis@gmail.com>
2017-01-10 09:48:24 -0500

    track notifications sent/failed

--- app/models/events/notification_events/failed_event.rb
+++ app/models/events/notification_events/failed_event.rb
@@ -3,13 +3,21 @@ module Events
 
         class FailedEvent < NotificationEvent
 
-        	def self.event_id
+            Events::Pipeline.register("notification", "failed", self, { targetable: false })
+
+            def self.event_id
                 Events::Constants::NOTIFICATION_FAILED_EVENT_ID
             end
 
-            Events::Pipeline.register("notification", "failed", self, { targetable: false })
-            
-        end
+            after_save :update_stats
 
+            def update_stats
+                if message
+                    counters = { total_notifications_failed: 1 }
+                    MessageTemplateStats.update_counters(message.message_template_id, counters)
+                end
+            end
+
+        end
     end
 end
--- app/models/events/notification_events/sent_event.rb
+++ app/models/events/notification_events/sent_event.rb
@@ -3,13 +3,21 @@ module Events
 
         class SentEvent < NotificationEvent
 
-        	def self.event_id
+            Events::Pipeline.register("notification", "sent", self, { targetable: false })
+
+            def self.event_id
                 Events::Constants::NOTIFICATION_SENT_EVENT_ID
             end
 
-            Events::Pipeline.register("notification", "sent", self, { targetable: false })
+            after_save :update_stats
+
+            def update_stats
+                if message
+                    counters = { total_notifications_sent: 1 }
+                    MessageTemplateStats.update_counters(message.message_template_id, counters)
+                end
+            end
             
         end
-
     end
 end
