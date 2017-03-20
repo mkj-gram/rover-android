e97a18495b2dc37cc86e79238a64ba458cdde7e1
alex graham <graham.ajk@gmail.com>
2017-03-20 16:37:49 -0400

    Add campaign types and Dockerfile

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import express from 'express'
 import graphqlHTTP from 'express-graphql'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
 
 import schema from './schema'
 
+dotenv.config()
+
+
 const app = express()
 
-app.use('/graphql', graphqlHTTP({
+app.set('port', (process.env.PORT || 80))
+
+app.use(morgan('tiny'))
+
+app.use('/graphql', cors(), graphqlHTTP({
     schema,
     graphiql: true
 }))
 
-app.listen(4000)
+app.listen(app.get('port'))
 console.log('Running a GraphQL API server at localhost:4000/graphql')
