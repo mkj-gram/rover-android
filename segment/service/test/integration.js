478291a1b618d2eeab5c5c88edd41f965d068c63
Chris Recalis <crecalis@gmail.com>
2017-06-02 15:43:32 -0400

    add template for integration test

---
+++ segment/service/test/integration.js
@@ -0,0 +1,30 @@
+var rewire = require("rewire")
+var sshandler = rewire('../handlers/v1/static-segment-handler')
+var expect = require('expect.js')
+
+
+describe('listStaticSegments', function() {
+	
+});
+
+
+describe('getStaticSegment', function() {
+	
+});
+
+
+describe('createStaticSegment', function() {
+	
+});
+
+describe('destroyStaticSegment', function() {
+	
+});
+
+describe('updateStaticSegmentPushIds', function() {
+	
+});
+
+describe('getStaticSegmentPushIds', function() {
+	
+});
