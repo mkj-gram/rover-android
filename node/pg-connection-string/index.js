ee336733753abcc8ae92fa0de0509bf4608a069f
Chris Recalis <crecalis@gmail.com>
2017-06-08 09:31:22 -0400

    [node-common] Add pg-connection-string parse library

---
+++ node/pg-connection-string/index.js
@@ -0,0 +1,90 @@
+const URL = require('url')
+const QUERYSTRING = require('querystring')
+const fs = require('fs')
+
+const enabledSSLModes = ['allow', 'prefer', 'require', 'verify-ca', 'verify-full']
+
+function isEmpty(obj) {
+    return Object.keys(obj).length == 0
+}
+
+function parse(connection) {
+
+    if (connection == undefined || connection == null) {
+        return {}
+    }
+
+    let url = URL.parse(connection)
+
+    if (url.protocol != 'postgres:') {
+        throw new TypeError("protocol must be postgres but got: " + url.protocol)
+    }
+
+    let parsedConnection = {}
+
+    if (url.host) {
+        parsedConnection.host = url.host
+    }
+
+    if (url.port) {
+        parsedConnection.port = parseInt(url.port)
+    }
+
+    if (url.auth) {
+        let auth = url.auth.split(":")
+        let user = auth[0]
+        let password = auth[1]
+
+        parsedConnection.user = user
+        parsedConnection.password = password
+    }
+
+    if (url.pathname) {
+        let dbname = url.pathname.substring(1)
+        parsedConnection.database = dbname
+    }
+
+
+    if (url.query) {
+        let query = QUERYSTRING.parse(url.query)
+
+        /*
+            https://www.postgresql.org/docs/9.6/static/libpq-connect.html
+            Default is prefer
+         */
+        
+        let sslmode = query.sslmode || 'prefer'
+
+        let shouldEnableSSL = enabledSSLModes.includes(sslmode)
+
+        if (shouldEnableSSL || query.sslrootcert || query.sslkey || query.sslcert) {
+            
+            let sslConnection = {}
+
+            if (query.sslrootcert) {
+                sslConnection.ca = fs.readFileSync(query.sslrootcert)
+            }
+
+            if (query.sslkey) {
+                sslConnection.key = fs.readFileSync(query.sslkey)
+            }
+
+            if (query.sslcert) {
+                sslConnection.cert = fs.readFileSync(query.sslcert)
+            }
+
+            if (isEmpty(sslConnection)) {
+                parsedConnection.ssl = true
+            } else {
+                parsedConnection.ssl = sslConnection
+            }
+        } else {
+            parsedConnection.ssl = false
+        }
+    }
+
+    return parsedConnection
+}
+
+
+module.exports = parse
---
+++ node/pg-connection-string/test/test.js
@@ -0,0 +1,121 @@
+const parse = require('../index')
+const assert = require('assert')
+const path = require('path')
+const fs = require('fs')
+
+const testfile = path.join(__dirname, "test.txt")
+const testfileContents = "abc"
+
+fs.writeFileSync(testfile, testfileContents)
+
+describe('parse', function() {
+    
+    after(function() {
+        fs.unlinkSync(testfile)
+    })
+
+
+    it('throws a type error when the protocol is not postgres', function() {
+        try {
+            parse("")
+            throw new Error("TypeError was not thrown")
+        } catch (e) {
+            assert.equal(e instanceof TypeError, true)
+        }
+    })
+
+    it('parses the host name', function() {
+        let dsn = "postgres://postgres@localhost/"
+        let conn = parse(dsn)
+
+        assert.equal(conn.host, "localhost")
+    })
+
+    it('parses the port', function() {
+        let dsn = "postgres://postgres@localhost:123/"
+        let conn = parse(dsn)
+
+        assert.equal(conn.port, 123)
+    })
+
+    it('parses the dbname', function() {
+        let dsn = "postgres://postgres@localhost:123/hello123"
+        let conn = parse(dsn)
+
+        assert.equal(conn.database, 'hello123')
+    })
+
+    it('parses authentication', function() {
+        let dsn = "postgres://username:password@localhost:123/"
+        let conn = parse(dsn)
+
+        assert.equal(conn.user, "username")
+        assert.equal(conn.password, "password")
+    })
+
+    describe('ssl', function() {
+
+        it('returns false when sslmode is set to disable', function() {
+            let dsn = "postgres://postgres@localhost:123/?sslmode=disable"
+            let conn = parse(dsn)
+
+            assert.equal(conn.ssl, false)
+        })
+
+        it('returns true when ssl is enabled but no other options are passed', function() {
+            let dsn = "postgres://postgres@localhost:123/?sslmode=verify-ca"
+            let conn = parse(dsn)
+            
+            assert.equal(conn.ssl, true)
+        })
+
+        it('parses sslcert', function() {
+            let dsn = `postgres://postgres@localhost:123/?sslmode=verify-ca&sslcert=${testfile}`
+            let conn = parse(dsn)
+            
+            assert.equal(typeof conn.ssl, 'object')
+
+            let certContents = conn.ssl.cert.toString()
+            
+            assert.equal(certContents, testfileContents)
+        })
+
+        it('parses sslrootcert', function() {
+            let dsn = `postgres://postgres@localhost:123/?sslmode=verify-ca&sslrootcert=${testfile}`
+            let conn = parse(dsn)
+            
+            assert.equal(typeof conn.ssl, 'object')
+
+            let certContents = conn.ssl.ca.toString()
+            
+            assert.equal(certContents, testfileContents)
+        })
+
+        it('parses sslkey', function() {
+            let dsn = `postgres://postgres@localhost:123/?sslmode=verify-ca&sslkey=${testfile}`
+            let conn = parse(dsn)
+            
+            assert.equal(typeof conn.ssl, 'object')
+
+            let keyContents = conn.ssl.key.toString()
+            
+            assert.equal(keyContents, testfileContents)
+        })
+
+        it('parses sslcert, sslrootcert, sslkey', function() {
+            let dsn = `postgres://postgres@localhost:123/?sslmode=verify-ca&sslkey=${testfile}&sslcert=${testfile}&sslrootcert=${testfile}`
+            let conn = parse(dsn)
+            
+            assert.equal(typeof conn.ssl, 'object')
+
+            let rootCertContents = conn.ssl.ca.toString()
+            let certContents = conn.ssl.cert.toString()
+            let keyContents = conn.ssl.key.toString()
+            
+            assert.equal(rootCertContents, testfileContents)
+            assert.equal(certContents, testfileContents)
+            assert.equal(keyContents, testfileContents)
+            
+        });
+    })
+})
