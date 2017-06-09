df7ccc5954d55d1a8ba1c3bc81f99c162430264a
Chris Recalis <crecalis@gmail.com>
2017-06-09 00:31:59 -0400

    [rest-gateway] initial commit

---
+++ rest-gateway/index.js
@@ -0,0 +1,73 @@
+const http = require('http')
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const grpcCodes = require("@rover-common/grpc-codes")
+
+/* Setup Auth Client and Middleware */
+const Auth = require('@rover/auth-client')
+const AuthClient = Auth.v1.Client()
+const AuthMiddleware = Auth.v1.Middleware(AuthClient)
+
+/* Setup Express Server */
+const app = express()
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(AuthMiddleware)
+app.disable('x-powered-by');
+
+/* Load Controllers */
+
+const staticSegmentController = require('./routes/static-segments')
+
+app.use('/v1/static-segments', staticSegmentController)
+
+/* Add healthcheck endpoint for kubernetes */
+app.get('/healthcheck', function(req, res) {
+    res.writeHead(200)
+    return res.end()
+})
+
+/* Catch all requests we can't support and return 404 */
+app.use(function(req, res, next) {
+    const err = new Error("Not Found")
+    err.status = 404
+    return next(err)
+})
+
+
+/* Error Handling */
+app.use(function(err, req, res, next) {
+    // if an error has a code it most likely a grpc error code
+    
+    if (err && err.code) {
+        err.status = grpcCodes.grpcToHttp(err.code)
+    }
+
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+    // render the error page
+    res.status(err.status || 500)
+    return res.end()
+})
+
+/* Server Boot */
+
+function normalizePort() {
+    if (isNaN(process.PORT)) {
+        return 3000
+    }
+
+    return parseInt(process.env.PORT)
+}
+
+const port = normalizePort()
+
+app.listen(port, function () {
+    console.log('Rest-Gateway listening on port: ' + port)
+})
---
+++ rest-gateway/routes/static-segments.js
@@ -0,0 +1,180 @@
+const express = require('express')
+const router = express.Router()
+
+const RoverApis = require("@rover/apis")
+const SegmentClient = require("@rover/segment-client").v1.Client()
+
+
+function makeError(status, message) {
+    const error = new Error(message)
+    error.status = status
+    return error
+}
+
+
+function mustAuthenticate(req, res, next) {
+    if (req.authContext == null || req.authContext == undefined) {
+        return next(makeError(401, "Unauthorized"))
+    }
+
+    return next()
+}
+
+/*
+    We force all routes inside this router to be authenticated. If we can't authenticate with auth
+    we just throw and Unauthorized error which will be picked up by the main router's error logic
+ */
+router.use(mustAuthenticate)
+
+/* 
+    ROUTE: /v1/static-segments
+    SCOPES: server, admin
+*/
+
+function getISOString(timestamp) {
+    let date = new Date(1970, 0, 1)
+    date.setSeconds = timestamp.getSeconds()
+    return date.toISOString()
+}
+
+function serialize(segment) {
+    return {
+        'id': segment.getId(),
+        'type': "static-segments",
+        'attributes': {
+            'title': segment.getTitle(),
+            'size': segment.getSize(),
+            'updated-at': getISOString(segment.getUpdatedAt()),
+            'created-at': getISOString(segment.getCreatedAt())
+        }
+    }
+}
+
+/**
+ *  List all static segments
+ */
+router.get('/', function(req, res, next) {
+
+    const request = new RoverApis.segment.v1.Models.ListStaticSegmentRequest()
+
+    request.setAuthContext(req._authContext)
+    request.setAccountId(requ.authContext.account_id)
+
+    if (req.query.sort) {
+        request.setOrderBy(req.query.sort)
+    } else {
+        request.setOrderBy("created_at DESC")
+    }
+
+    // Default for now. We can customize later if we want to support pagination
+    // http://jsonapi.org/format/#fetching-pagination
+    request.setPageSize(500)
+
+    SegmentClient.listStaticSegments(request, function(err, response) {
+        if (err) {
+            return next(err)
+        }
+
+        const segments = response.getSegmentsList()
+
+        const jsonResponse = {
+            data: segments.map(serialize)
+        }
+
+        return res.json(jsonResponse)
+    })
+})
+
+/**
+ *  Returns a single static segment
+ */
+router.get('/:id', function(req, res, next) {
+
+    const id = parseInt(req.params.id)
+
+    if (isNaN(id)) {
+        return next(makeError(400, "Id must be an integer but got " + req.params.id))
+    }
+
+    const request = new RoverApis.segment.v1.Models.GetStaticSegmentRequest()
+
+    request.setAuthContext(req._authContext)
+    request.setId(id)
+
+    SegmentClient.getStaticSegment(request, function(err, response) {
+        if (err) {
+            return next(err)
+        }
+
+        const jsonResponse = {
+            data: serialize(response.getSegment())
+        }
+
+        return res.json(jsonResponse)
+
+    })
+})
+
+/**
+ *  Updates a single static segment
+ */
+router.post('/', function(req, res, next) {
+
+    // Parsed json body
+    const body = request.body
+
+    /* 
+        Example JSON why are we using json api spec again?
+        {
+          "data": {
+            "type": "static-segments",
+            "attributes": {
+              "title": "Ember Hamster"
+            }
+          }
+        }
+    */
+   
+    if (body.data == undefined || body.null) {
+        return next(makeError(400, "Invalid JSON API"))
+    }
+
+    if (body.data.type != 'static-segments') {
+        return next(makeError(400, "Invalid type"))
+    }
+
+    if (body.data.attributes == undefined || body.data.attributes == null) {
+        return next(makeError(400, "Attributes cannot be left blank"))
+    }
+
+    if (body.data.attributes.title == undefined || body.data.attributes.title == null) {
+        return next(makeError(400, "Title must be set and cannot be null"))
+    }
+
+    const request = new RoverApis.segment.v1.Models.CreateStaticSegmentRequest()
+
+    
+    request.setAuthContext(req._authContext)
+    request.setTitle(body.data.attributes.title)
+
+    SegmentClient.createStaticSegment(request, function(err, response) {
+        if (err) {
+            return next(err)
+        }
+
+        const jsonResponse = {
+            data: serialize(response)
+        }
+
+        return res.json(jsonResponse)
+    })
+})
+
+/**
+ *  Deletes a single static segment
+ */
+router.delete('/:id', function(req, res, next) {
+    return next(makeError(501, "Not Implemented yet"))
+})
+
+module.exports = router
