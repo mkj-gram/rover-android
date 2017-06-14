4b985796e5e96d289a79560555dba5bc9847f892
Chris Recalis <crecalis@gmail.com>
2017-06-14 14:52:55 -0400

    [rest-gateway] Support application/vnd.api+json

--- rest-gateway/index.js
+++ rest-gateway/index.js
@@ -22,7 +22,7 @@ if (Config.get('/raven/enabled')) {
 }
 app.use(cors())
 app.use(logger('dev'))
-app.use(bodyParser.json())
+app.use(bodyParser.json({ type: ['application/json', 'application/vnd.api+json'] }))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(AuthMiddleware)
 app.disable('x-powered-by');
