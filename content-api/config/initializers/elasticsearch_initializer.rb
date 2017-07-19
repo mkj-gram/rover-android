26c9096d885fce5b644ca10ae48e08c992f227e9
chrisrecalis <chrisrecalis@users.noreply.github.com>
2017-07-19 15:45:36 -0400

    Remove tmp ssl workaround (#143)
    The elasticsearch cluster now uses `LestsEncrypt` so there is no need to use self signed certificates anymore

--- content-api/config/initializers/elasticsearch_initializer.rb
+++ content-api/config/initializers/elasticsearch_initializer.rb
@@ -1,2 +1,2 @@
 urls = Rails.configuration.elasticsearch["urls"]
-Elasticsearch::Model.client = Elasticsearch::Client.new(hosts: urls, transport_options: { ssl: { verify: false }})
+Elasticsearch::Model.client = Elasticsearch::Client.new(hosts: urls)
