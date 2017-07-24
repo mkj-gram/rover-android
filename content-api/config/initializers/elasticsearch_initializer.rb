1fe78d72e30f0775856a7558350366b215b2eb34
chrisrecalis <chrisrecalis@users.noreply.github.com>
2017-07-24 09:36:20 -0400

    timeout requests taking longer than 30 seconds (#162)

--- content-api/config/initializers/elasticsearch_initializer.rb
+++ content-api/config/initializers/elasticsearch_initializer.rb
@@ -1,2 +1,2 @@
 urls = Rails.configuration.elasticsearch["urls"]
-Elasticsearch::Model.client = Elasticsearch::Client.new(hosts: urls)
+Elasticsearch::Model.client = Elasticsearch::Client.new(hosts: urls, request_timeout: 30)
