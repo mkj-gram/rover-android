e556064d838eaa4773c4044bf9b6772b7313c1ff
Will Yang <willyyang604@gmail.com>
2017-07-27 11:39:24 -0400

    Update trash and rename icons

--- src/TrashIcon.js
+++ src/TrashIcon.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default props =>
-    <svg {...props} width="16" height="16" viewBox="0 0 16 16">
-        <path fillRule="evenodd" d="M10.5,1 L9.5,0 L6.5,0 L5.5,1 L2,1 L2,3 L14,3 L14,1 L10.5,1 Z M3,4 L13,4 L13,13.9914698 C13,15.1007504 12.1125667,16 11.000385,16 L4.99961498,16 C3.89525812,16 3,15.0980496 3,13.9914698 L3,4 Z"/>
+export default props => 
+    <svg {...props} width="12" height="16" viewBox="0 0 12 16">
+        <path fill={props.fill} fillRule="evenodd" d="M8.5,1 L12,1 L12,3 L0,3 L0,1 L3.5,1 L4.5,0 L7.5,0 L8.5,1 Z M1,4 L11,4 L11,13.9914698 C11,15.1007504 10.1125667,16 9.00038502,16 L2.99961498,16 C1.89525812,16 1,15.0980496 1,13.9914698 L1,4 Z"/>
     </svg>
