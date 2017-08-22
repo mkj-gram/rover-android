9e66210a770b00a0abfbb658a4923da261875a4e
chrisrecalis <chrisrecalis@users.noreply.github.com>
2017-08-22 16:15:40 -0400

    [go] Add newrelic server middleware (#240)
    * [go] Add newrelic server middleware
    
    * [go] newrelic middleware detect grpc errors

---
+++ go/grpc/middleware/newrelic/server_interceptors.go
@@ -0,0 +1,30 @@
+package grpc_newrelic
+
+import (
+	"github.com/newrelic/go-agent"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
+	"google.golang.org/grpc/codes"
+)
+
+func UnaryServerInterceptor(app newrelic.Application) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		txn := app.StartTransaction(info.FullMethod, nil, nil)
+		defer txn.End()
+		newrelicCtx := context.WithValue(ctx, "newrelic-txn", txn)
+		response, err := handler(newrelicCtx, req)
+		if err != nil {
+			if grpcError, ok := status.FromError(err); ok {
+				code := grpcError.Code()
+				if code == codes.Unknown || code == codes.Internal || code == codes.DataLoss || code == codes.Unavailable {
+					txn.NoticeError(err)
+				}
+			} else {
+				txn.NoticeError(err)
+			}
+		}
+
+		return response, err
+	}
+}
