41cc60650cb974d64142f545417105d53765c0ef
Will Yang <willyyang604@gmail.com>
2017-09-14 18:12:45 -0400

    Update error catch on auth failure

--- src/relay/Environment.js
+++ src/relay/Environment.js
@@ -1,38 +1,48 @@
 /* eslint-disable */
-const {
-    Environment,
-    Network,
-    RecordSource,
-    Store
-} = require('relay-runtime')
+const { Environment, Network, RecordSource, Store } = require('relay-runtime')
 
 const store = new Store(new RecordSource())
 
 const uri = {
-    'production': 'https://api.rover.io/graphql',
-    'staging': 'https://api.staging.rover.io/graphql',
-    'development': 'http://localhost:4000/graphql'
+    production: 'https://api.rover.io/graphql',
+    staging: 'https://api.staging.rover.io/graphql',
+    development: 'http://localhost:4000/graphql'
 }
 
 const getToken = () => {
     const session = JSON.parse(
         localStorage.getItem('ember_simple_auth:session')
     )
+
     return session ? session.authenticated.token : null
 }
 
-const network = Network.create((operation, variables) => fetch(uri[process.env.NODE_ENV], {
+const network = Network.create(async (operation, variables) => {
+    const response = await fetch(uri[process.env.NODE_ENV], {
         method: 'POST',
         headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json',
-            'Authorization': `Bearer: ${getToken()}`
+            Authorization: `Bearer: ${getToken()}`
         },
         body: JSON.stringify({
             query: operation.text,
             variables
         })
-    }).then(response => response.json()))
+    })
+
+    const data = await response.json()
+
+    if (
+        data.hasOwnProperty('errors') &&
+        data.errors[0].message === 'Unauthenticated'
+    ) {
+        window.location.replace('/auth/sign-in')
+        return
+    } else {
+        return data
+    }
+})
 
 const environment = new Environment({
     network,
