61bbb79f0b02571bfc2a73cea5132942fc732317
Sean Rucker <sean@rover.io>
2017-10-02 14:26:17 -0400

    Admin fields on experience

--- graphql-gateway/src/experiences/models/Experience.js
+++ graphql-gateway/src/experiences/models/Experience.js
@@ -1,5 +1,7 @@
 import {
+    GraphQLBoolean,
     GraphQLID,
+    GraphQLInt,
     GraphQLList,
     GraphQLNonNull,
     GraphQLObjectType,
@@ -11,17 +13,73 @@ import Screen from './Screen'
 const Experience = new GraphQLObjectType({
     name: 'Experience',
     fields: () => ({
+        hasUnpublishedChanges: {
+            type: new GraphQLNonNull(GraphQLBoolean),
+            resolve: data => {
+                // TODO: Verify admin scope
+                return data['has-unpublished-changes']
+            }
+        },
         id: {
             type: new GraphQLNonNull(GraphQLID),
             resolve: data => data['id']
+        },
+        isArchived: {
+            type: new GraphQLNonNull(GraphQLBoolean),
+            resolve: data => {
+                // TODO: Verify admin scope
+                return data['is-archived']
+            }
+        },
+        isPublished: {
+            type: new GraphQLNonNull(GraphQLBoolean),
+            resolve: data => {
+                // TODO: Verify admin scope
+                return data['is-published']
+            }
         },
         homeScreenId: {
             type: new GraphQLNonNull(GraphQLString),
             resolve: data => data['home-screen-id']
         },
+        name: {
+            type: new GraphQLNonNull(GraphQLString),
+            resolve: data => {
+                // TODO: Verify admin scope
+                return data['name']
+            }
+        },
+        recentAverageDuration: {
+            type: GraphQLInt,
+            resolve: data => {
+                // TODO: Verify admin scope
+                return data['recent-average-duraction']
+            }
+        },
         screens: {
             type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(Screen))),
             resolve: data => data['screens']
+        },
+        simulatorUrl: {
+            type: new GraphQLNonNull(GraphQLString),
+            resolve: data => {
+                // TODO: Verify admin scope
+                return data['simulator-url']
+            }
+        },
+        versionId: {
+            type: GraphQLString,
+            resolve: data => {
+                // TODO: Verify admin scope
+                return data['version-id']
+            }
+        },
+        viewToken: {
+            type: new GraphQLNonNull(GraphQLString),
+            resolve: data => {
+                // TODO: Verify admin scope
+                return data['view-token']
+            }
         }
     })
 })
