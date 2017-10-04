e2e088b7c399b586a9db349ae5f2069f78643fb5
alexjgraham <alexjgraham@users.noreply.github.com>
2017-10-04 18:56:38 -0400

    [graphql-gateway] mock regions and profiles typo fixes (#481)
    * [graphql-gateway] export regions and profile marie
    
    * [graphql-gateway] acccount -> account

--- graphql-gateway/src/device/fixtures/profiles.js
+++ graphql-gateway/src/device/fixtures/profiles.js
@@ -1,21 +1,19 @@
-export default ({
-                    "marie": {
-                        "identifier": "80000516109",
-                        "attributes": {
-                            "firstName": "Marie",
-                            "lastName": "Avgeropoulos",
-                            "email": "marie.avgeropoulos@example.com",
-                            "gender": "female",
-                            "age": 30,
-                            "phoneNumber": "555-555-5555",
-                            "tags": [
-                                "actress",
-                                "model",
-                                "musician"
-                            ],
-                            "height": 1.65
-                        },
-                        "createdAt": "2017-10-04T13:54:16-04:00",
-                        "updatedAt": "2017-10-04T13:54:16-04:00"
-                    }
-                })
+export const marie = {
+    identifier: "80000516109",
+    attributes: {
+        firstName: "Marie",
+        lastName: "Avgeropoulos",
+        email: "marie.avgeropoulos@example.com",
+        gender: "female",
+        age: 30,
+        phoneNumber: "555-555-5555",
+        tags: [
+            "actress",
+            "model",
+            "musician"
+        ],
+        height: 1.65
+    },
+    createdAt: "2017-10-04T13:54:16-04:00",
+    updatedAt: "2017-10-04T13:54:16-04:00"
+}
--- graphql-gateway/src/device/fixtures/regions.js
+++ graphql-gateway/src/device/fixtures/regions.js
@@ -1,171 +1,169 @@
-export default ({
-                "geofenceRegions": [
+export const geofenceRegions = [
                     {
-                        "latitude": 43.649503,
-                        "longitude": -79.376955,
-                        "radius": 50
+                        latitude: 43.649503,
+                        longitude: -79.376955,
+                        radius: 50
                     },
                     {
-                        "latitude": 43.677718,
-                        "longitude": -79.62482,
-                        "radius": 3700
+                        latitude: 43.677718,
+                        longitude: -79.62482,
+                        radius: 3700
                     },
                     {
-                        "latitude": 43.819772,
-                        "longitude": -79.179957,
-                        "radius": 50
+                        latitude: 43.819772,
+                        longitude: -79.179957,
+                        radius: 50
                     },
                     {
-                        "latitude": 43.917635,
-                        "longitude": -78.960376,
-                        "radius": 200
+                        latitude: 43.917635,
+                        longitude: -78.960376,
+                        radius: 200
                     },
                     {
-                        "latitude": 43.928498,
-                        "longitude": -78.877542,
-                        "radius": 50
+                        latitude: 43.928498,
+                        longitude: -78.877542,
+                        radius: 50
                     },
                     {
-                        "latitude": 43.915976,
-                        "longitude": -78.962034,
-                        "radius": 200
+                        latitude: 43.915976,
+                        longitude: -78.962034,
+                        radius: 200
                     },
                     {
-                        "latitude": 43.626259,
-                        "longitude": -79.514748,
-                        "radius": 50
+                        latitude: 43.626259,
+                        longitude: -79.514748,
+                        radius: 50
                     },
                     {
-                        "latitude": 43.645303,
-                        "longitude": -79.380457,
-                        "radius": 200
+                        latitude: 43.645303,
+                        longitude: -79.380457,
+                        radius: 200
                     },
                     {
-                        "latitude": 43.865016,
-                        "longitude": -78.938177,
-                        "radius": 50
+                        latitude: 43.865016,
+                        longitude: -78.938177,
+                        radius: 50
                     },
                     {
-                        "latitude": 43.921982,
-                        "longitude": -78.928977,
-                        "radius": 150
+                        latitude: 43.921982,
+                        longitude: -78.928977,
+                        radius: 150
                     },
                     {
-                        "latitude": 43.641438,
-                        "longitude": -79.389353,
-                        "radius": 150
+                        latitude: 43.641438,
+                        longitude: -79.389353,
+                        radius: 150
                     },
                     {
-                        "latitude": 43.649318,
-                        "longitude": -79.382493,
-                        "radius": 50
+                        latitude: 43.649318,
+                        longitude: -79.382493,
+                        radius: 50
                     },
                     {
-                        "latitude": 43.646365,
-                        "longitude": -79.402312,
-                        "radius": 50
+                        latitude: 43.646365,
+                        longitude: -79.402312,
+                        radius: 50
                     },
                     {
-                        "latitude": 43.64006,
-                        "longitude": -79.419813,
-                        "radius": 50
+                        latitude: 43.64006,
+                        longitude: -79.419813,
+                        radius: 50
                     },
                     {
-                        "latitude": 43.65109,
-                        "longitude": -79.389905,
-                        "radius": 50
+                        latitude: 43.65109,
+                        longitude: -79.389905,
+                        radius: 50
                     },
                     {
-                        "latitude": 43.593704,
-                        "longitude": -79.631093,
-                        "radius": 50
+                        latitude: 43.593704,
+                        longitude: -79.631093,
+                        radius: 50
                     },
                     {
-                        "latitude": 43.95951,
-                        "longitude": -78.945787,
-                        "radius": 50
+                        latitude: 43.95951,
+                        longitude: -78.945787,
+                        radius: 50
                     },
                     {
-                        "latitude": 43.961211,
-                        "longitude": -78.975767,
-                        "radius": 50
+                        latitude: 43.961211,
+                        longitude: -78.975767,
+                        radius: 50
                     },
                     {
-                        "latitude": 43.661896,
-                        "longitude": -79.385748,
-                        "radius": 50
+                        latitude: 43.661896,
+                        longitude: -79.385748,
+                        radius: 50
                     },
                     {
-                        "latitude": 43.646882,
-                        "longitude": -79.400586,
-                        "radius": 350
+                        latitude: 43.646882,
+                        longitude: -79.400586,
+                        radius: 350
                     },
                     {
-                        "latitude": 43.644629,
-                        "longitude": -79.392933,
-                        "radius": 50
+                        latitude: 43.644629,
+                        longitude: -79.392933,
+                        radius: 50
                     },
                     {
-                        "latitude": 43.641246,
-                        "longitude": -79.41468,
-                        "radius": 50
+                        latitude: 43.641246,
+                        longitude: -79.41468,
+                        radius: 50
                     },
                     {
-                        "latitude": 43.650678,
-                        "longitude": -79.375814,
-                        "radius": 50
+                        latitude: 43.650678,
+                        longitude: -79.375814,
+                        radius: 50
                     }
-                ],
-                "beaconRegions": [
+                ]
+export const beaconRegions = [
                     {
-                        "uuid": "2EE4FA7F-6AF2-4F85-95CF-71D17C7D6B4D",
+                        uuid: "2EE4FA7F-6AF2-4F85-95CF-71D17C7D6B4D",
                         "major-number": null,
                         "minor-number": null
                     },
                     {
-                        "uuid": "6C21D507-C7F7-42C5-BA24-ADF3010BC612",
+                        uuid: "6C21D507-C7F7-42C5-BA24-ADF3010BC612",
                         "major-number": null,
                         "minor-number": null
                     },
                     {
-                        "uuid": "B9407F30-F5F8-466E-AFF9-25556B57FE6D",
+                        uuid: "B9407F30-F5F8-466E-AFF9-25556B57FE6D",
                         "major-number": null,
                         "minor-number": null
                     },
                     {
-                        "uuid": "FC733542-62E4-46FD-8DF1-438D6ECE3DD1",
+                        uuid: "FC733542-62E4-46FD-8DF1-438D6ECE3DD1",
                         "major-number": null,
                         "minor-number": null
                     },
                     {
-                        "uuid": "B435CE53-9EC2-4B20-99D1-070FEF1CDA2E",
+                        uuid: "B435CE53-9EC2-4B20-99D1-070FEF1CDA2E",
                         "major-number": null,
                         "minor-number": null
                     },
                     {
-                        "uuid": "2EE4FA7F-6AF2-4F85-95CF-71D17C7D6B4D",
+                        uuid: "2EE4FA7F-6AF2-4F85-95CF-71D17C7D6B4D",
                         "major-number": null,
                         "minor-number": null
                     },
                     {
-                        "uuid": "BE209EF1-5B7B-4CD4-9059-D825418A0D71",
+                        uuid: "BE209EF1-5B7B-4CD4-9059-D825418A0D71",
                         "major-number": 18696,
                         "minor-number": 26589
                     },
                     {
-                        "uuid": "BE209EF1-5B7B-4CD4-9059-D825418A0D71",
+                        uuid: "BE209EF1-5B7B-4CD4-9059-D825418A0D71",
                         "major-number": 18696,
                         "minor-number": 26590
                     },
                     {
-                        "uuid": "BE209EF1-5B7B-4CD4-9059-D825418A0D71",
+                        uuid: "BE209EF1-5B7B-4CD4-9059-D825418A0D71",
                         "major-number": 18696,
                         "minor-number": 26591
                     },
                     {
-                        "uuid": "BE209EF1-5B7B-4CD4-9059-D825418A0D71",
+                        uuid: "BE209EF1-5B7B-4CD4-9059-D825418A0D71",
                         "major-number": 18696,
                         "minor-number": 26592
                     }
                 ]
-            })
