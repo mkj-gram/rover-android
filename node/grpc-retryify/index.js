2341085c1c987103fda1dba049fd241412e2383b
chrisrecalis <chrisrecalis@users.noreply.github.com>
2017-10-16 10:54:04 -0400

    [node] Add grpc-retryify (#501)
    [node] Add grpc-retryify

---
+++ node/grpc-retryify/index.js
@@ -0,0 +1,78 @@
+var retry = require('retry')
+
+/**
+ * Default retry configuration
+ * @type {Object}
+ */
+const defaults = {
+    retries: 5,
+    factor: 2,
+    minTimeout: 1 * 1000,
+    maxTimeout: 60 * 1000,
+    randomize: false,
+}
+
+/**
+ * GRPC error codes which are retryable
+ * @type {Array}
+ */
+const codes = [2, 8, 10, 13, 14]
+
+/**
+ * Checks if the error is retryable
+ * @param  {Error} err 
+ * @return {Boolean}
+ */
+function retryableError(err) {
+    if (err === null || err === undefined) {
+        return false
+    }
+
+    if (err.code === null || err.code === undefined) {
+        return false
+    }
+
+    return codes.includes(err.code)
+}
+
+function retryify(client) {
+    Object.keys(Object.getPrototypeOf(client)).forEach(functionName => {
+        const originalFunction = client[functionName]
+
+        client[functionName] = (...args) => {
+            let callOpts = args[1]
+            // Callback is the original callback
+            let callback = args[args.length - 1]
+
+            let overrides = typeof callOpts === 'object' && typeof callOpts['retry'] === 'object' && callOpts['retry']
+            let retryOpts = Object.assign({}, defaults, overrides)
+
+            let requestArgs = args
+            if (typeof requestArgs[requestArgs.length - 1] === 'function') {
+                // We are replacing the callback with our own
+                requestArgs = requestArgs.slice(0, requestArgs.length - 1)
+            }
+
+            let operation = retry.operation(retryOpts)
+
+            operation.attempt(function(currentAttempt) {
+                originalFunction.call(client, ...requestArgs, function(err, response) {
+                    
+                    // Abort early if its not retryable
+                    if (err && !retryableError(err)) {
+                        operation.stop()
+                        return callback(err)
+                    }
+
+                    if (operation.retry(err)) {
+                        return
+                    }
+
+                    return callback(err ? operation.mainError() : null, response)
+                })
+            })
+        }
+    })
+}
+
+module.exports = retryify
---
+++ node/grpc-retryify/test/test.js
@@ -0,0 +1,122 @@
+const assert = require('sinon').assert
+const expect = require('chai').expect
+const sinon = require('sinon')
+const retry = require('../index.js')
+
+describe('retryify', function() {
+		
+	function makeClient() {
+		function Client() {}
+		Client.prototype.test = function(req, cb) { return cb(req) }
+
+		return new Client()
+	}
+
+	it('wraps all prototype functions', function() {
+		let spy = sinon.spy()
+
+		let client = makeClient()
+		retry(client)
+
+		client.test("request", spy)
+
+		assert.calledOnce(spy)
+
+	})
+
+	it('preserves calling arguments', function() {
+		let spy = sinon.spy()
+
+		let client = makeClient()
+		retry(client)
+
+		client.test("request", spy)
+
+		assert.calledWith(spy, "request")
+	})
+
+	it('returns success after 3 failed attempts', function(done) {
+		let client = makeClient()
+		let stub = sinon.stub(client, 'test')
+		retry(client)
+		
+
+		// First call returns a grpc error
+		stub.onCall(0).callsFake(function(req, opts, cb) {
+			return cb({ code: 2 })
+		})
+
+		// Second call returns a grpc error
+		stub.onCall(1).callsFake(function(req, opts, cb) {
+			return cb({ code: 2 })
+		})
+
+		stub.onCall(2).callsFake(function(req, opts, cb) {
+			return cb(null, "a")
+		})
+
+		client.test("a", { retry: { factor: 1, minTimeout: 10 }}, function(err, response) {
+			expect(err).to.be.null
+			expect(response).to.equal("a")
+			done()
+		})
+
+	}).timeout(5000)
+
+	it('aborts early when the error is non retryable', function(done) {
+		let client = makeClient()
+		let stub = sinon.stub(client, 'test')
+		retry(client)
+		
+
+		// First call returns a grpc error
+		stub.onCall(0).callsFake(function(req, cb) {
+			return cb(new Error("Blow Up!"))
+		})
+
+		// Second call returns a grpc error
+		stub.onCall(1).callsFake(function(req, cb) {
+			return cb({ code: 2 })
+		})
+
+		stub.onCall(2).callsFake(function(req, cb) {
+			return cb(null, "a")
+		})
+
+		client.test("a", function(err, response) {
+			expect(err).to.be.an('error')
+			expect(response).to.be.undefined
+			assert.calledOnce(stub)
+			done()
+		})
+	})
+
+
+	it('returns an error when number of operations were exhausted', function(done) {
+		let client = makeClient()
+		let stub = sinon.stub(client, 'test')
+		retry(client)
+		
+
+		// First call returns a grpc error
+		stub.onCall(0).callsFake(function(req, cb) {
+			return cb(new Error("Blow Up!"))
+		})
+
+		// Second call returns a grpc error
+		stub.onCall(1).callsFake(function(req, cb) {
+			return cb({ code: 2 })
+		})
+
+		stub.onCall(2).callsFake(function(req, cb) {
+			return cb(null, "a")
+		})
+
+		client.test("a", function(err, response) {
+			expect(err).to.be.an('error')
+			expect(response).to.be.undefined
+			assert.calledOnce(stub)
+			done()
+		})
+	})
+})
