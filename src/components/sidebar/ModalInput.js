1713bdbb2990826e7ef7e95724083dc5267816e1
alex graham <graham.ajk@gmail.com>
2017-11-06 12:42:36 -0500

    remove autofocus modalinput

--- src/components/sidebar/ModalInput.js
+++ src/components/sidebar/ModalInput.js
@@ -25,14 +25,12 @@ const ModalInput = (props) => {
     }
     const onFocus = (e) => {
         e.target.style.borderColor = silver
-        const length = e.target.value.length
-        e.target.setSelectionRange(length, length)
+        e.target.value = ''
     }
     const onBlur = e => (e.target.style.borderColor = steel)
 
     return (
         <input
-            autoFocus
             {...props}
             style={inputStyle}
             onFocus={onFocus}
