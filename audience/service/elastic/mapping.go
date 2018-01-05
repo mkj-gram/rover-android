c8fef2404a5b324402e91667f6f4a80c613d394c
Chris Recalis <crecalis@gmail.com>
2018-01-05 11:39:12 -0500

    [audience/service] Add v2 device mapping
    - Device’s now have their profile embedded

--- audience/service/elastic/mapping.go
+++ audience/service/elastic/mapping.go
@@ -2,6 +2,110 @@ package elastic
 
 import "github.com/roverplatform/rover/audience/service/mongodb"
 
+func DeviceV2Mapping(customDeviceAttributesMapping, customProfileAttributesMapping M) M {
+
+	return M{
+		"device": M{
+			// Meta
+			// https://www.elastic.co/guide/en/elasticsearch/reference/5.5/mapping-all-field.html
+			"_all": M{"enabled": false},
+			"properties": M{
+				"account_id": M{"type": "integer"},
+
+				"device_id": M{"type": "keyword"},
+
+				"profile_identifier": M{"type": "keyword"},
+
+				// NOTE: ms-12: embedded profile
+				"profile": M{
+					"properties": profileProperties(customProfileAttributesMapping),
+				},
+
+				// NOTE: ms-12: embedded attributes
+				"attributes": M{
+					"properties": customDeviceAttributesMapping,
+				},
+
+				"created_at": M{"type": "date"},
+				"updated_at": M{"type": "date"},
+
+				"is_test_device": M{"type": "boolean"},
+				"label":          M{"type": "string"},
+
+				"push_environment":           M{"type": "keyword"},
+				"push_token_key":             M{"type": "keyword"},
+				"push_token_is_active":       M{"type": "boolean"},
+				"push_token_created_at":      M{"type": "date"},
+				"push_token_updated_at":      M{"type": "date"},
+				"push_token_unregistered_at": M{"type": "date"},
+
+				"app_name":            M{"type": "keyword"},
+				"app_version":         M{"type": "keyword"},
+				"app_build":           M{"type": "keyword"},
+				"app_namespace":       M{"type": "keyword"},
+				"device_manufacturer": M{"type": "keyword"},
+				"device_model":        M{"type": "keyword"},
+				"os_name":             M{"type": "keyword"},
+				//
+				"os_version": M{
+					"properties": M{
+						"major":    M{"type": "integer"},
+						"minor":    M{"type": "integer"},
+						"revision": M{"type": "integer"},
+					},
+				},
+
+				"locale_language": M{"type": "keyword"},
+				"locale_region":   M{"type": "keyword"},
+				"locale_script":   M{"type": "keyword"},
+
+				"is_wifi_enabled":     M{"type": "boolean"},
+				"is_cellular_enabled": M{"type": "boolean"},
+
+				"screen_width":  M{"type": "integer"},
+				"screen_height": M{"type": "integer"},
+
+				"carrier_name": M{"type": "keyword"},
+				"radio":        M{"type": "keyword"},
+				"time_zone":    M{"type": "keyword"},
+				"platform":     M{"type": "keyword"},
+
+				"is_background_enabled":          M{"type": "boolean"},
+				"is_location_monitoring_enabled": M{"type": "boolean"},
+				"is_bluetooth_enabled":           M{"type": "boolean"},
+
+				"advertising_id": M{"type": "keyword"},
+
+				"ip": M{"type": "ip"},
+
+				"notification_authorization": M{"type": "keyword"},
+
+				// https://www.elastic.co/guide/en/elasticsearch/reference/5.5/geo-point.html
+				"location": M{"type": "geo_point"},
+
+				"location_latitude":  M{"type": "double"},
+				"location_longitude": M{"type": "double"},
+
+				"location_accuracy": M{"type": "integer"},
+
+				"location_country":    M{"type": "keyword"},
+				"location_state":      M{"type": "keyword"},
+				"location_city":       M{"type": "keyword"},
+				"location_updated_at": M{"type": "date"},
+
+				// maps to frameworks[io.rover.Rover]
+				"sdk_version": M{
+					"properties": M{
+						"major":    M{"type": "integer"},
+						"minor":    M{"type": "integer"},
+						"revision": M{"type": "integer"},
+					},
+				},
+			},
+		},
+	}
+}
+
 func DeviceMapping() M {
 	return M{
 		"device": M{
@@ -142,7 +246,7 @@ func ProfileMapping(profileAttributesMapping M) M {
 	}
 }
 
-func ProfileAttributesMapping(attrs []*mongodb.SchemaAttribute) M {
+func CustomAttributesMapping(attrs []*mongodb.SchemaAttribute) M {
 	var mapping = M{}
 
 	for _, attr := range attrs {
@@ -168,4 +272,18 @@ func ProfileAttributesMapping(attrs []*mongodb.SchemaAttribute) M {
 	}
 
 	return mapping
+}
+
+func profileProperties(mapping M) M {
+	return M{
+		"id":         M{"type": "keyword"}, // {"$oid": "aaaaaaaaaaaaaaaaaaaaaa01"},
+		"account_id": M{"type": "integer"},
+		"identifier": M{"type": "keyword"}, // "78e19dbf-8c0b-47a5-b28f-444444444444",
+		"created_at": M{"type": "date"},    // {"$date": "2016-08-22T19:05:53.102Z"},
+		"updated_at": M{"type": "date"},    // {"$date": "2016-08-22T19:05:53.102Z"},
+		"attributes": M{
+			"properties": mapping,
+		},
+	}
+
 }
