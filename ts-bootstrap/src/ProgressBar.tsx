57a03ed91d56752248657a3f6e9fe404c8e9bb2a
alex graham <graham.ajk@gmail.com>
2018-01-12 17:20:45 -0500

    [ts-bootstrap] ProgressBar has container

--- ts-bootstrap/src/ProgressBar.tsx
+++ ts-bootstrap/src/ProgressBar.tsx
@@ -1,12 +1,13 @@
 /// <reference path="../typings/index.d.ts"/>
 import * as React from 'react'
-import { cloud } from '../styles/colors'
+import { cloud, almostWhite } from '../styles/colors'
 
 type ProgressBarProps = {
     children?: JSX.Element
     progress: number
     style: {
         barStyle?: React.CSSProperties
+        containerStyle?: React.CSSProperties
         progressStyle?: React.CSSProperties
     }
 }
@@ -16,24 +17,33 @@ const ProgressBar: React.SFC<ProgressBarProps> = ({
     style,
     children
 }: ProgressBarProps) => {
-    const barStyle = {
+    const containerStyle: React.CSSProperties = {
+        height: 24,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        ...style.containerStyle
+    }
+    const barStyle: React.CSSProperties = {
         overflow: 'hidden' as 'hidden',
         width: 240,
-        height: 8,
+        height: 12,
         backgroundColor: cloud,
-        borderRadius: 4,
+        borderRadius: 12,
         ...style.barStyle
     }
-    const progressStyle = {
+    const progressStyle: React.CSSProperties = {
         backgroundColor: 'tomato',
         width: progress > 3 ? `${progress}%` : barStyle.height,
         height: '100%',
-        borderRadius: 8,
+        borderRadius: 12,
         ...style.progressStyle
     }
     return (
-        <div style={barStyle}>
-            <div style={progressStyle}>{children}</div>
+        <div style={containerStyle}>
+            <div style={barStyle}>
+                <div style={progressStyle}>{children}</div>
+            </div>
         </div>
     )
 }
