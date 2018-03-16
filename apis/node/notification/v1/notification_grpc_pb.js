c1f6cc15a4fc416dedbb99168cadaa3e9472b428
Maryan Hratson <mhratson@users.noreply.github.com>
2018-03-16 12:34:29 -0400

    [protos][apis] notification service definitions (#879)
    - only what's related to sending Campaigns push
    * [protos][apis] notifcation service definitions
    * [apis] notification service
    * [apis] notification service mocks

---
+++ apis/node/notification/v1/notification_grpc_pb.js
@@ -0,0 +1,45 @@
+// GENERATED CODE -- DO NOT EDIT!
+
+'use strict';
+var grpc = require('grpc');
+var notification_v1_notification_pb = require('../../notification/v1/notification_pb.js');
+var auth_v1_auth_pb = require('../../auth/v1/auth_pb.js');
+
+function serialize_rover_notification_v1_SendCampaignNotificationRequest(arg) {
+  if (!(arg instanceof notification_v1_notification_pb.SendCampaignNotificationRequest)) {
+    throw new Error('Expected argument of type rover.notification.v1.SendCampaignNotificationRequest');
+  }
+  return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_rover_notification_v1_SendCampaignNotificationRequest(buffer_arg) {
+  return notification_v1_notification_pb.SendCampaignNotificationRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_rover_notification_v1_SendCampaignNotificationResponse(arg) {
+  if (!(arg instanceof notification_v1_notification_pb.SendCampaignNotificationResponse)) {
+    throw new Error('Expected argument of type rover.notification.v1.SendCampaignNotificationResponse');
+  }
+  return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_rover_notification_v1_SendCampaignNotificationResponse(buffer_arg) {
+  return notification_v1_notification_pb.SendCampaignNotificationResponse.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+
+var NotificationService = exports.NotificationService = {
+  sendCampaignNotification: {
+    path: '/rover.notification.v1.Notification/SendCampaignNotification',
+    requestStream: false,
+    responseStream: false,
+    requestType: notification_v1_notification_pb.SendCampaignNotificationRequest,
+    responseType: notification_v1_notification_pb.SendCampaignNotificationResponse,
+    requestSerialize: serialize_rover_notification_v1_SendCampaignNotificationRequest,
+    requestDeserialize: deserialize_rover_notification_v1_SendCampaignNotificationRequest,
+    responseSerialize: serialize_rover_notification_v1_SendCampaignNotificationResponse,
+    responseDeserialize: deserialize_rover_notification_v1_SendCampaignNotificationResponse,
+  },
+};
+
+exports.NotificationClient = grpc.makeGenericClientConstructor(NotificationService);
--- protos/builds/scripts/gomocks.sh
+++ protos/builds/scripts/gomocks.sh
@@ -1,17 +1,28 @@
-WORKDIR=/go/src/github.com/roverplatform/rover
-MOCKGEN="go run /go/src/github.com/roverplatform/rover/vendor/github.com/golang/mock/mockgen/*.go"
+#!/bin/sh
+
+set -e
+
+export PATH=/go/bin:$PATH
+
+mkdir -p /go/bin
+mkdir -p /go/src/github.com/golang
+ln -s /go/src/github.com/roverplatform/rover/vendor/github.com/golang/mock /go/src/github.com/golang/
+go build -o /go/bin/mockgen github.com/golang/mock/mockgen
 
 cd $WORKDIR
 
 # Audience Client Mock
 mkdir -p apis/go/audience/v1/mock
-$MOCKGEN  -package mock -destination apis/go/audience/v1/mock/mock.go github.com/roverplatform/rover/apis/go/audience/v1 AudienceClient
-
+mockgen  -package mock -destination apis/go/audience/v1/mock/mock.go github.com/roverplatform/rover/apis/go/audience/v1 AudienceClient
 
 # Campaigns Client Mock
 mkdir -p apis/go/campaigns/v1/mock
-$MOCKGEN  -package mock -destination apis/go/campaigns/v1/mock/mock.go github.com/roverplatform/rover/apis/go/campaigns/v1 CampaignsClient
+mockgen  -package mock -destination apis/go/campaigns/v1/mock/mock.go github.com/roverplatform/rover/apis/go/campaigns/v1 CampaignsClient
 
 # Geocoder Client Mock
 mkdir -p apis/go/geocoder/v1/mock
-$MOCKGEN -package mock -destination apis/go/geocoder/v1/mock/mock.go github.com/roverplatform/rover/apis/go/geocoder/v1  GeocoderClient
+mockgen -package mock -destination apis/go/geocoder/v1/mock/mock.go github.com/roverplatform/rover/apis/go/geocoder/v1  GeocoderClient
+
+# Notification Client Mock
+mkdir -p apis/go/notification/v1/mock
+mockgen -package mock -destination apis/go/notification/v1/mock/mock.go github.com/roverplatform/rover/apis/go/notification/v1  NotificationClient
