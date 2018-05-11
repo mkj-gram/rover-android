20f727b5575550b8075ce13babf547acf2233fd9
Maryan Hratson <mhratson@users.noreply.github.com>
2018-05-11 15:13:22 -0400

    [docker] more monoimages (#1117)
    - auth, audience
    - unify notification,campaigns images

--- auth/service/scripts/create_account.sh
+++ auth/service/scripts/create_account.sh
@@ -4,6 +4,6 @@ set -e
 
 DCS='docker-compose -f ../../docker/main.docker-compose.yaml'
 
-ACCOUNT_ID=$(${DCS} run --rm -T -e NAME=${NAME} -e PASSWORD=${PASSWORD} -e EMAIL=${EMAIL} auth-service ./docker/auth-service/cmd create_account)
+ACCOUNT_ID=$(${DCS} run --rm -T -e NAME=${NAME} -e PASSWORD=${PASSWORD} -e EMAIL=${EMAIL} auth-service ./docker/auth/cmd create_account)
 
-${DCS} run --rm -e ACCOUNT_ID=${ACCOUNT_ID} audience-service ./docker/audience-service/cmd index
+${DCS} run --rm -e ACCOUNT_ID=${ACCOUNT_ID} audience-service ./docker/audience/cmd index
