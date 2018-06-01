b11e6241ed810f340e57e8f4a482c5bdedcc229e
chrisrecalis <chrisrecalis@users.noreply.github.com>
2018-06-01 11:18:59 -0400

    [go/postgres-cli] Add create db method (#1261)

--- go/cmd/postgres-cli/main.go
+++ go/cmd/postgres-cli/main.go
@@ -1,41 +1,125 @@
 package main
 
 import (
-	"log"
-
 	"database/sql"
-	_ "github.com/lib/pq"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
 
+	"github.com/lib/pq"
 	"github.com/namsral/flag"
+	"github.com/pkg/errors"
 	"github.com/pressly/goose"
 )
 
-var migration struct {
+var (
+	createCmd  = flag.NewFlagSet("create", flag.ExitOnError)
+	migrateCmd = flag.NewFlagSet("migrate", flag.ExitOnError)
+
+	/* CreateCmd flags */
+	createDbDsn = createCmd.String("db-dsn", "postgres://postgres@postgres:5432?sslmode=disable", "database url")
+
+	/* MigrateCmd flags */
+	migrateDbDsn   = migrateCmd.String("db-dsn", ``, "db DSN")
+	migratePath    = migrateCmd.String("migration-path", ``, "path to migrations")
+	migrateCommand = migrateCmd.String("migration-cmd", `up`, "migration cmd")
+	migrateArgs    = migrateCmd.String("migration-args", ``, "migration args")
+)
+
+type Command interface {
+	Run() error
+}
+
+type CreateCommand struct {
+	DSN string
+}
+
+func (c CreateCommand) Run() error {
+	u, err := url.Parse(c.DSN)
+	if err != nil {
+		return err
+	}
+
+	if len(u.Path) == 0 {
+		return errors.New("db name cannot be empty")
+	}
+	var dbName = u.Path[1:]
+	// Strip path to login into default db
+	u.Path = ""
+	db, err := sql.Open("postgres", u.String())
+	if err != nil {
+		return errors.Wrap(err, "sql.Open")
+	}
+
+	_, err = db.Exec(fmt.Sprintf("CREATE DATABASE %s", dbName))
+	if err != nil {
+		// 42P04 -> duplicate_database error code
+		if pqErr, ok := err.(*pq.Error); ok && pqErr.Code != "42P04" || !ok {
+			return errors.Wrap(err, "db.Exec")
+		}
+	}
+
+	return nil
+}
+
+type MigrationCommand struct {
 	Path string
 	Cmd  string
 	Args string
 	DSN  string
 }
 
+func (c MigrationCommand) Run() error {
+	db, err := sql.Open("postgres", c.DSN)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Migrations: cmd=%q path=%q\n", c.Cmd, c.Path)
+	goose.Status(db, c.Path)
+
+	if err := goose.Run(c.Cmd, db, c.Path, c.Args); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func main() {
 
-	flag.StringVar(&migration.DSN, "db-dsn", ``, "db DSN")
-
-	flag.StringVar(&migration.Path, "migration-path", ``, "path to migrations")
-	flag.StringVar(&migration.Cmd, "migration-cmd", `up`, "migration cmd")
-	flag.StringVar(&migration.Args, "migration-args", ``, "migration args")
-
-	flag.Parse()
-
-	db, err := sql.Open("postgres", migration.DSN)
-	if err != nil {
-		log.Fatalln("sql.Open", err)
+	if len(os.Args) < 2 {
+		// If command is not specified default to migrate
+		os.Args = append(os.Args, "migrate")
 	}
 
-	log.Printf("Migrations: cmd=%q path=%q\n", migration.Cmd, migration.Path)
-	goose.Status(db, migration.Path)
+	var cmd Command
 
-	if err := goose.Run(migration.Cmd, db, migration.Path, migration.Args); err != nil {
-		log.Fatalln("goose.Run:", err)
+	switch os.Args[1] {
+	case "create":
+		createCmd.Parse(os.Args[2:])
+	case "migrate":
+		migrateCmd.Parse(os.Args[2:])
 	}
+
+	if createCmd.Parsed() {
+		cmd = &CreateCommand{
+			DSN: *createDbDsn,
+		}
+	}
+
+	if migrateCmd.Parsed() {
+		cmd = &MigrationCommand{
+			DSN:  *migrateDbDsn,
+			Cmd:  *migrateCommand,
+			Args: *migrateArgs,
+			Path: *migratePath,
+		}
+	}
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("success")
 }
