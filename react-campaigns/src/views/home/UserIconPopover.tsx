6b9067ea3ff505a74115b1578059ef4127b2fd9f
Will Yang <willy.yang@mail.utoronto.ca>
2018-06-29 14:36:36 -0400

    [react-campaigns] Update user icon popover UI (#1564)
    * [react-campaigns] Update user icon popover UI

--- react-campaigns/src/views/home/UserIconPopover.tsx
+++ react-campaigns/src/views/home/UserIconPopover.tsx
@@ -1,5 +1,6 @@
 /// <reference path="../../../typings/index.d.ts" />
 import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 import { connect, Dispatch } from 'react-redux'
 
 import {
@@ -7,14 +8,28 @@ import {
     UserIcon,
     charcoal,
     Text,
-    Button,
     aquamarine,
-    turquoise
+    turquoise,
+    ExternalLinkIcon,
+    titanium,
+    steel,
+    white,
+    cloud
 } from '@rover/ts-bootstrap/dist/src'
 
-import { updateActivePopover, fetchUser } from '../../actions'
+import MobilePopover from '../wizards/components/MobilePopover'
 
-import { getActivePopover, getUser } from '../../reducers'
+import {
+    updateActivePopover,
+    fetchUser,
+    closePopoverModalForm
+} from '../../actions'
+
+import {
+    getActivePopover,
+    getUser,
+    getIsPopoverModalFormOpen
+} from '../../reducers'
 
 export interface Props {
     device: Media
@@ -23,53 +38,213 @@ export interface Props {
 export interface UserIconPopoverProps {
     activePopover: string
     user: User
+    isPopoverModalFormOpen: string
 }
 
 export interface UserIconPopoverDispatchProps {
     updateActivePopover: (field: string) => void
     fetchUser: () => void
+    closePopoverModalForm: () => void
+}
+
+export interface UserIconPopoverState {
+    profileMouseEventDirection: 'up' | 'down'
+    settingsMouseEventDirection: 'up' | 'down'
 }
 
 class UserIconPopover extends React.Component<
     UserIconPopoverProps & UserIconPopoverDispatchProps & Props,
-    {}
+    UserIconPopoverState
 > {
     constructor(
         props: UserIconPopoverProps & UserIconPopoverDispatchProps & Props
     ) {
         super(props)
-        this.getToken = this.getToken.bind(this)
+        this.state = {
+            profileMouseEventDirection: 'up',
+            settingsMouseEventDirection: 'up'
+        }
         this.handleSignOut = this.handleSignOut.bind(this)
+        this.cancelProfileSettingSelection = this.cancelProfileSettingSelection.bind(
+            this
+        )
     }
 
     componentDidMount() {
         this.props.fetchUser()
     }
 
-    getToken() {
-        const session = JSON.parse(
-            localStorage.getItem('ember_simple_auth:session')
-        )
-        return session ? session.authenticated.token : null
-    }
-
     handleSignOut() {
         localStorage.removeItem('ember_simple_auth:session')
-        if (!this.getToken()) {
-            window.location.replace('/auth/sign-in')
-        }
+        window.location.replace('/auth/sign-in')
+    }
+
+    cancelProfileSettingSelection() {
+        const {
+            closePopoverModalForm,
+            device,
+            updateActivePopover
+        } = this.props
+
+        closePopoverModalForm()
+        setTimeout(() => updateActivePopover(''), device === 'Mobile' ? 500 : 0)
     }
 
     render() {
-        const { activePopover, updateActivePopover, device, user } = this.props
+        const {
+            activePopover,
+            updateActivePopover,
+            device,
+            user,
+            isPopoverModalFormOpen
+        } = this.props
         const { userName, userEmail, accountName } = user
+        const {
+            profileMouseEventDirection,
+            settingsMouseEventDirection
+        } = this.state
 
-        return (
+        const getUserIconInformation = () => (
+            <div
+                style={{
+                    width: device !== 'Mobile' ? 384 : '100%',
+                    paddingLeft: device !== 'Mobile' ? 16 : 24,
+                    paddingRight: device !== 'Mobile' ? 16 : 24,
+                    display: 'flex',
+                    flexDirection: 'column'
+                }}
+            >
+                <div
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                        height: device !== 'Mobile' ? 80 : 96,
+                        borderBottom: `1px solid ${titanium}`
+                    }}
+                >
+                    <div style={{ display: 'flex', flexDirection: 'column' }}>
+                        <Text size="medium" text={userName} />
+                        <Text
+                            label={true}
+                            size="small"
+                            text={userEmail}
+                            textStyle={{ color: steel }}
+                        />
+                    </div>
+                    {device === 'Desktop' && (
+                        <div
+                            onMouseDown={() =>
+                                this.setState({
+                                    profileMouseEventDirection: 'down'
+                                })
+                            }
+                            onMouseUp={() => {
+                                this.setState({
+                                    profileMouseEventDirection: 'up'
+                                })
+                                window.location.replace('/settings/overview')
+                            }}
+                            style={{
+                                display: 'flex',
+                                alignItems: 'center'
+                            }}
+                        >
+                            <Text
+                                text="Profile"
+                                size="medium"
+                                textStyle={{
+                                    marginRight: 8,
+                                    fontWeight: 600,
+                                    color:
+                                        profileMouseEventDirection === 'up'
+                                            ? turquoise
+                                            : aquamarine
+                                }}
+                            />
+                            <ExternalLinkIcon
+                                fill={
+                                    profileMouseEventDirection === 'up'
+                                        ? turquoise
+                                        : aquamarine
+                                }
+                            />
+                        </div>
+                    )}
+                </div>
+                <div
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                        height: device !== 'Mobile' ? 56 : 72,
+                        borderBottom:
+                            device === 'Mobile' ? `1px solid ${titanium}` : null
+                    }}
+                >
+                    <Text size="medium" text={accountName} />
+                    {device === 'Desktop' && (
+                        <div
+                            onMouseDown={() =>
+                                this.setState({
+                                    settingsMouseEventDirection: 'down'
+                                })
+                            }
+                            onMouseUp={() => {
+                                this.setState({
+                                    settingsMouseEventDirection: 'up'
+                                })
+                                window.location.replace('/settings/overview')
+                            }}
+                            style={{
+                                display: 'flex',
+                                alignItems: 'center'
+                            }}
+                        >
+                            <Text
+                                text="Settings"
+                                size="medium"
+                                textStyle={{
+                                    marginRight: 8,
+                                    fontWeight: 600,
+                                    color:
+                                        settingsMouseEventDirection === 'up'
+                                            ? turquoise
+                                            : aquamarine
+                                }}
+                            />
+                            <ExternalLinkIcon
+                                fill={
+                                    settingsMouseEventDirection === 'up'
+                                        ? turquoise
+                                        : aquamarine
+                                }
+                            />
+                        </div>
+                    )}
+                </div>
+            </div>
+        )
+        const navBarProps = {
+            buttonLeft: 'Done',
+            buttonLeftCallback: this.cancelProfileSettingSelection,
+            buttonRight: 'Sign Out',
+            buttonRightCallback: this.handleSignOut,
+            title: device === 'Mobile' ? 'Account' : ''
+        }
+
+        return device !== 'Mobile' ? (
             <PopoverContainer
                 id="user-icon-home-page"
                 popoverProps={{
                     placement:
-                        device === 'Desktop' ? 'bottom-end' : 'bottom-start'
+                        device === 'Desktop' ? 'bottom-end' : 'bottom-start',
+                    navBarProperties: navBarProps,
+                    arrowColors: {
+                        primary: cloud,
+                        secondary: white,
+                        border: titanium
+                    }
                 }}
                 onClick={() =>
                     activePopover === 'user-icon-home-page-popover'
@@ -78,9 +253,6 @@ class UserIconPopover extends React.Component<
                 }
                 targetParent="root"
                 showPopover={activePopover === 'user-icon-home-page-popover'}
-                style={{
-                    height: 20
-                }}
             >
                 <UserIcon
                     fill={charcoal}
@@ -88,106 +260,40 @@ class UserIconPopover extends React.Component<
                     width={device === 'Desktop' ? '20' : '24'}
                     viewBox="0 0 24 24"
                 />
-                <div
-                    style={{
-                        width: 192,
-                        padding: 16,
-                        display: 'flex',
-                        flexDirection: 'column'
-                    }}
-                >
-                    <div
-                        style={{
-                            display: 'flex'
-                        }}
-                    >
-                        <Text
-                            text="Account Name :"
-                            label={true}
-                            size="small"
-                            textStyle={{ marginRight: 8 }}
-                        />
-                        <Text
-                            text={accountName}
-                            size="medium"
-                            textStyle={{
-                                whiteSpace: 'nowrap',
-                                overflow: 'hidden',
-                                textOverflow: 'ellipsis'
-                            }}
-                        />
-                    </div>
 
-                    <div
-                        style={{
-                            display: 'flex'
-                        }}
-                    >
-                        <Text
-                            text="Name :"
-                            label={true}
-                            size="small"
-                            textStyle={{ marginRight: 8 }}
-                        />
-                        <Text
-                            text={userName}
-                            size="medium"
-                            textStyle={{
-                                whiteSpace: 'nowrap',
-                                overflow: 'hidden',
-                                textOverflow: 'ellipsis'
-                            }}
-                        />
-                    </div>
-
-                    <div
-                        style={{
-                            display: 'flex'
-                        }}
-                    >
-                        <Text
-                            text="Email :"
-                            label={true}
-                            size="small"
-                            textStyle={{ marginRight: 8 }}
-                        />
-                        <Text
-                            text={userEmail}
-                            size="medium"
-                            textStyle={{
-                                whiteSpace: 'nowrap',
-                                overflow: 'hidden',
-                                textOverflow: 'ellipsis'
-                            }}
-                        />
-                    </div>
-                    <div
-                        style={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            marginTop: 8
-                        }}
-                    >
-                        <Button
-                            text="Sign Out"
-                            type="regular"
-                            mouseDownColors={{
-                                active: turquoise,
-                                inactive: aquamarine
-                            }}
-                            onClick={() => this.handleSignOut()}
-                        />
-                    </div>
-                </div>
+                {getUserIconInformation()}
             </PopoverContainer>
+        ) : (
+            <div
+                onClick={() =>
+                    updateActivePopover('user-icon-home-page-popover')
+                }
+            >
+                <UserIcon
+                    fill={charcoal}
+                    height="24"
+                    width="24"
+                    viewBox="0 0 24 24"
+                />
+                {activePopover === 'user-icon-home-page-popover' &&
+                    ReactDOM.createPortal(
+                        <MobilePopover
+                            animation={isPopoverModalFormOpen}
+                            navbarProps={navBarProps}
+                        >
+                            {getUserIconInformation()}
+                        </MobilePopover>,
+                        document.getElementById('root')
+                    )}
+            </div>
         )
     }
 }
 
 const mapStateToProps = (state: State): UserIconPopoverProps => ({
     activePopover: getActivePopover(state),
-    user: getUser(state)
+    user: getUser(state),
+    isPopoverModalFormOpen: getIsPopoverModalFormOpen(state)
 })
 const mapDispatchToProps = (
     // tslint:disable-next-line:no-any
@@ -196,7 +302,8 @@ const mapDispatchToProps = (
     return {
         updateActivePopover: (activePopover: string) =>
             dispatch(updateActivePopover(activePopover)),
-        fetchUser: () => dispatch(fetchUser())
+        fetchUser: () => dispatch(fetchUser()),
+        closePopoverModalForm: () => dispatch(closePopoverModalForm())
     }
 }
 export default connect(
