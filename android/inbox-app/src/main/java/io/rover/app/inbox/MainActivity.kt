284db52eebd4da1e374180ee3bdb696f596e3300
Andrew Clunis <andrew@orospakr.ca>
2018-07-12 16:39:25 -0400

    [android] Enable location features in the inbox app with a permissions prompt.

--- android/inbox-app/src/main/java/io/rover/app/inbox/MainActivity.kt
+++ android/inbox-app/src/main/java/io/rover/app/inbox/MainActivity.kt
@@ -1,8 +1,13 @@
 package io.rover.app.inbox
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
 import android.view.Menu
 import android.view.MenuItem
 import io.rover.account.AuthService
@@ -40,6 +45,8 @@ class MainActivity : AppCompatActivity() {
 
         deviceIdentifier.text =
             Rover.sharedInstance.resolveSingletonOrFail(DeviceIdentificationInterface::class.java).installationIdentifier
+
+        makePermissionsAttempt()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -60,4 +67,52 @@ class MainActivity : AppCompatActivity() {
 
         return super.onOptionsItemSelected(item)
     }
+
+    private fun makePermissionsAttempt() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                AlertDialog.Builder(this)
+                    .setMessage("My App would like to use your location for Geofences and Beacons.")
+                    .setNeutralButton("Got it") { _, _ ->
+                        makePermissionsAttempt()
+                    }
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    0
+                )
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+            Rover.sharedInstance.permissionsNotifier.permissionGranted(
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        val perms = permissions.zip(grantResults.toList()).associate { it }
+
+        if(perms[Manifest.permission.ACCESS_FINE_LOCATION] == PackageManager.PERMISSION_GRANTED) {
+            Rover.sharedInstance.permissionsNotifier.permissionGranted(
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        }
+    }
+
 }
